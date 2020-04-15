b226508d2882eddc04d584034d662680e59e9e82
Bart Heupers <bart.heupers@gmail.com>
2020-04-15 11:32:37 +0200

    Added primary key and checks

--- src/dags/grex.py
+++ src/dags/grex.py
@@ -11,11 +11,17 @@ from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.postgres_operator import PostgresOperator
+from postgres_check_operator import PostgresCheckOperator, PostgresValueCheckOperator
 from airflow.operators.python_operator import PythonOperator
 
 from common import default_args
 from swift_operator import SwiftOperator
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+from common.sql import (
+    SQL_CHECK_COUNT,
+    SQL_CHECK_GEO,
+)
 
 
 def wkt_loads_wrapped(data):
@@ -47,21 +53,25 @@ def load_grex(input_csv, table_name):
         encoding="latin_1",
         parse_dates=["STARTDATUM"],
         dayfirst=True,
+        index_col="PLANNR",
     )
     df.rename(columns=lambda x: x.lower(), inplace=True)
+    df.index.name = "id"
     df["geometrie"] = df["geometrie"].apply(wkt_loads_wrapped)
 
     grex_rapportage_dtype = {
-        "plannr": Integer(),
+        "id": Integer(),
         "plannaam": Text(),
         "planbeheercode": Text(),
         "startdatum": Date(),
         "planstatus": Text(),
         "oppervlakte": Float(),
-        "geometrie": Geometry(geometry_type='MULTIPOLYGON', srid=4326),
+        "geometrie": Geometry(geometry_type="MULTIPOLYGON", srid=4326),
     }
 
     df.to_sql(table_name, db_engine, if_exists="replace", dtype=grex_rapportage_dtype)
+    with db_engine.connect() as connection:
+        connection.execute("ALTER TABLE grex_new ADD PRIMARY KEY (id)")
 
 
 dag_id = "grex"
@@ -70,10 +80,11 @@ dag_config = Variable.get(dag_id, deserialize_json=True)
 table_name = dag_id
 table_name_new = f"{dag_id}_new"
 SQL_TABLE_RENAME = f"""
-    BEGIN;
-    DROP TABLE IF EXISTS grex CASCADE;
+    DROP TABLE IF EXISTS {table_name} CASCADE;
     ALTER TABLE {table_name_new} RENAME TO {table_name};
-    COMMIT;
+    ALTER TABLE {table_name} RENAME CONSTRAINT {table_name_new}_pkey TO grex_pkey;
+    ALTER INDEX ix_{table_name_new}_id RENAME TO ix_{table_name}_id;
+    ALTER INDEX idx_{table_name_new}_geometrie RENAME TO idx_{table_name}_geometrie;
 """
 
 
@@ -97,7 +108,21 @@ with DAG("grex", default_args=default_args, description="GrondExploitatie",) as 
         op_args=[f"{tmp_dir}/{csv_file}", table_name_new],
     )
 
+    check_count = PostgresCheckOperator(
+        task_id="check_count",
+        sql=SQL_CHECK_COUNT,
+        params=dict(tablename="grex_new", mincount=400),
+    )
+
+    # check_geo = PostgresCheckOperator(
+    #     task_id="check_geo",
+    #     sql=SQL_CHECK_GEO,
+    #     params=dict(
+    #         tablename="grex_new", geotype="ST_MultiPolygon", geo_column="geometrie"
+    #     ),
+    # )
+
     rename_table = PostgresOperator(task_id="rename_table", sql=SQL_TABLE_RENAME)
 
 
-mk_tmp_dir >> fetch_csv >> load_data >> rename_table
+mk_tmp_dir >> fetch_csv >> load_data >> check_count >> rename_table
