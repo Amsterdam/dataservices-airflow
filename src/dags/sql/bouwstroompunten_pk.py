a4390abed6771668a1316e55f42ab08b844b5ec8
Chris van Riel <c.van.riel@amsterdam.nl>
2020-07-13 08:19:34 +0200

    [DS-353] bouwstroompunten dataset initial setup
    Resolves: [DS-353]

---
+++ src/dags/sql/bouwstroompunten_pk.py
@@ -0,0 +1,5 @@
+# Because ogr2ogr cannot set a PK on an non-integer datatype, the PK is re-defined
+ADD_PK = """
+    ALTER TABLE {{ params.tablename }} DROP COLUMN ogc_fid;      
+    ALTER TABLE {{ params.tablename }} ADD CONSTRAINT "bouwstroompunten_new_pk" PRIMARY KEY ("id");    
+"""
