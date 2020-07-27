2d7cdf6ea00682068fd5f9ce2193246749cc0d28
cvriel <chrisvanriel@gmail.com>
2020-07-27 13:09:15 +0200

    Update bouwstroompunten.py

--- src/dags/bouwstroompunten.py
+++ src/dags/bouwstroompunten.py
@@ -62,9 +62,7 @@ def get_data():
     token_request = requests.post(
         token_url, data=json.dumps(token_payload), headers=token_headers, verify=False
     )
-    token_load = json.loads(token_request.text)
-    print(base_url)
-    print(token_load)
+    token_load = json.loads(token_request.text)    
     token_get = token_load["jwt"]
 
     # get data
