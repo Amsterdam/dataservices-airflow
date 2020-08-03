62e8c7d340e6e852c4bcc8ce05989b7a5791e6f8
cvriel <chrisvanriel@gmail.com>
2020-08-03 14:38:28 +0200

    Update bodem_set_geo_datatype.py

--- src/dags/sql/bodem_set_geo_datatype.py
+++ src/dags/sql/bodem_set_geo_datatype.py
@@ -1,5 +1,5 @@
 # Because geometry is ignore when importing .csv with ogr2ogr, it's set explicitly
 SET_GEOM = """
-    ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometry(POINT, 0) using ST_GeomFromText(CASE WHEN geometrie LIKE 'POINT%' THEN geometrie ELSE NULL END);   
+    ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometry(POINT, 28992) using ST_GeomFromText(CASE WHEN geometrie LIKE 'POINT%' THEN geometrie ELSE NULL END);   
     CREATE INDEX {{ params.tablename }}_idx ON {{ params.tablename }} USING GIST (geometrie);
 """
