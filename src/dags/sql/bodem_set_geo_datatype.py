71da6f68600a9a2e877d800c33827f3e9f7f92b5
cvriel <chrisvanriel@gmail.com>
2020-08-03 15:00:23 +0200

    Update bodem_set_geo_datatype.py

--- src/dags/sql/bodem_set_geo_datatype.py
+++ src/dags/sql/bodem_set_geo_datatype.py
@@ -2,5 +2,5 @@
 SET_GEOM = """
     ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometry(POINT, 0) using ST_GeomFromText(CASE WHEN geometrie LIKE 'POINT%' THEN geometrie ELSE NULL END);   
     CREATE INDEX {{ params.tablename }}_idx ON {{ params.tablename }} USING GIST (geometrie);
-    ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometrie(point, 28992) USING ST_SetSRID(geometrie, 28992);
+    ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometry(point, 28992) USING ST_SetSRID(geometrie, 28992);
 """
