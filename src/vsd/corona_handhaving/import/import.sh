b81a728760798d073f90e0e657648f2d19d6ea89
Chris van Riel <c.van.riel@amsterdam.nl>
2020-09-02 14:48:33 +0200

    [DS-425] added download logic oov brievenbus

--- src/vsd/corona_handhaving/import/import.sh
+++ src/vsd/corona_handhaving/import/import.sh
@@ -7,8 +7,22 @@ echo "Process import data"
 FILENAME=corona_handhaving.csv
 OBJECTSTORE_PATH=handhavingen/$FILENAME
 
-export AIRFLOW_CONN_SWIFT_DEFAULT=s3://${OS_USERNAME}:${OS_PASSWORD}@${OS_TENANT_NAME}
-echo "Download file from objectstore"
+echo "Download source file (csv) from oov.brievenbus.amsterdam.nl"
+# To test from localhost use SSH portforward from an acceptance machine
+# OOV_HOST="sftp://localhost"
+OOV_HOST=${OOV_BRIEVENBUS_HOST}
+OOV_PORT=${OOV_BRIEVENBUS_PORT}
+OOV_USER=${OOV_BRIEVENBUS_USER}
+OOV_PASSWORD=${OOV_BRIEVENBUS_PASSWORD}
+curl -k ${OOV_HOST}:${OOV_PORT}\/${FILENAME} --user ${OOV_USER}:${OOV_PASSWORD} -o "/tmp/corona_handhaving\/${FILENAME}" --create-dirs
+
+echo "Upload source file (csv) from oov.brievenbus.amsterdam.nl to Object Store"
+# URLdecode is needed for the password to pass
+alias urldecode='python3 -c "import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))"'
+OS_PASS_DECODE=$(urldecode ${OS_PASSWORD})
+curl -X PUT -T /tmp/corona_handhaving/${FILENAME} --user ${OS_USERNAME}:${OS_PASS_DECODE} https://${OS_TENANT_NAME}.objectstore.eu/handhavingen/${FILENAME}
+
+echo "Download file from objectstore as the startingpoint (the staging area) for processing"
 python $SHARED_DIR/utils/get_objectstore_file.py "$OBJECTSTORE_PATH"
 
 psql -X --set ON_ERROR_STOP=on << SQL
