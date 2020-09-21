387dbd7f52ee61a44b25b414b02e62b80d6bc110
Chris van Riel <c.van.riel@amsterdam.nl>
2020-09-21 14:09:40 +0200

    [fix] removing leading/trailing whitespaces corona handhaving data

--- src/vsd/corona_handhaving/import/import.py
+++ src/vsd/corona_handhaving/import/import.py
@@ -11,11 +11,24 @@ logging.basicConfig(level=LOGLEVEL)
 log = logging.getLogger(__name__)
 
 
+def strip(text):
+    """ removes leading and trailing whitespaces in data """
+    try:
+        return text.strip()
+    except AttributeError:
+        return text
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_csv", type=str, help="CSV file to process")
     args = parser.parse_args()
-    df = pd.read_csv(args.input_csv, sep=";")
+    df = pd.read_csv(
+        args.input_csv,
+        sep=";",
+        names=["organisatie", "type_interventie", "aantal", "week_nummer"],
+        converters={"organisatie": strip, "type_interventie": strip,},
+    )
     df.index.name = "id"
     engine = get_engine()
     df.to_sql(
