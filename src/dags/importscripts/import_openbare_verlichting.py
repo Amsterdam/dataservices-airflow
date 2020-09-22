b2f7ee56ea028bfe627c4355cd4e2a8b976c25ba
Chris van Riel <c.van.riel@amsterdam.nl>
2020-09-22 14:47:27 +0200

    [DS-395] added pipeline openbare verlichting

--- src/dags/importscripts/import_openbare_verlichting.py
+++ src/dags/importscripts/import_openbare_verlichting.py
@@ -6,39 +6,51 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # These types are checked against the data of the $OPENBARE_VERLICHTING_DATA_TYPES_SRC url.
-ID_TYPES_MAP = {
-    "1": {"external_name": "Klok", "internal_name": "Klok",},
-    "2": {"external_name": "Overspanning", "internal_name": "Overspanning",},
-    "3": {"external_name": "Gevel Armaturen", "internal_name": "Gevel_Armatuur",},
-    "4": {"external_name": "OVL Objecten", "internal_name": "Overig_lichtpunt",},
-    "5": {"external_name": "Grachtmast", "internal_name": "Grachtmast",},
-    "10": {"external_name": "Schijnwerpers", "internal_name": "Schijnwerpers",},
-}
+# Own administration of objecttypes is not used to compair with source administration.
+# To do: remove dictionary when SIA has confirmed
+# ID_TYPES_MAP = {
+#     "1": {"external_name": "Klok", "internal_name": "Klok",},
+#     "2": {"external_name": "Overspanning", "internal_name": "Overspanning",},
+#     "3": {"external_name": "Gevel Armaturen", "internal_name": "Gevel_Armatuur",},
+#     "4": {"external_name": "LSD Objecten", "internal_name": "Overig_lichtpunt",},
+#     "5": {"external_name": "Grachtmast", "internal_name": "Grachtmast",},
+#     "10": {"external_name": "Schijnwerpers", "internal_name": "Schijnwerpers",},
+# }
+
+ID_TYPES_MAP = {}
 
 
-def json2geojson(data,):
+def generate_source_types_mapping(types):
+    """ 
+    generates objecttypes mapping based on source file 
+    used in json2geojson to lookup name on objecttype id
+    i.e. ["1": {"code":"1","naam":"Klok"}, "10": {"code":"10","naam":"Schijnwerpers"}, ...]
+    """
+    for type in types:
+        ID_TYPES_MAP[type["code"]] = type
+
+
+def json2geojson(data, types):
+
     features = []
     for element in data:
         objecttype_id = element.get("objecttype")
+        type_map = ID_TYPES_MAP.get(objecttype_id)
         geometry = Point((element.get("lon"), element.get("lat")), srid=4326)
-
-        try:
-            type_mapping = ID_TYPES_MAP[objecttype_id]
-        except KeyError:
-            raise RuntimeError(
-                f"objecttype={objecttype_id} not found, unable to import {element!r}: "
-            ) from None
-        assert type_mapping is not None
-        type_name = type_mapping["internal_name"]
 
         features.append(
             {
                 "type": "Feature",
                 "geometry": geometry,
                 "properties": {
-                    "type_id": objecttype_id,
-                    "type_name": type_name,
-                    "objectnummer": element["objectnummer"],
+                    "object_id": element.get("objectid"),
+                    "objecttype": element.get("objecttype"),
+                    "objecttype_omschrijving": type_map["naam"],
+                    "objectnummer": element.get("objectnummer"),
+                    "breedtegraad": element.get("lat"),
+                    "lengtegraad": element.get("lon"),
+                    "storingstatus": element.get("storingstatus"),
+                    "meldingstatus": element.get("meldingstatus"),
                 },
             }
         )
@@ -49,34 +61,42 @@ def json2geojson(data,):
     return geojson
 
 
-def check_types_as_expected(types: list):
-    """
-    The transformation scripts runs under the assumption that the remote objects have a set mapping.
-    If that is not the case this function will detect that the local mapping is incorrect and will fail.
-    :param types: list, e.g. [{'code': '1', 'naam': 'Klok'}, ...]
-    :return:
-    """
-    actual_types = {
-        type["code"]: type["naam"] for type in types
-    }  # { '1': 'Klok', ... }
+# def check_types_as_expected(types: list):
+#     """
+#     The transformation scripts runs under the assumption that the remote objects have a set mapping.
+#     If that is not the case this function will detect that the local mapping is incorrect and will fail.
+#     :param types: list, e.g. [{'code': '1', 'naam': 'Klok'}, ...]
+#     :return:
+#     """
+#     actual_types = {
+#         type["code"]: type["naam"] for type in types
+#     }  # { '1': 'Klok', ... }
 
-    for code, mapping in ID_TYPES_MAP.items():
-        actual_type = actual_types.get(code)
-        assert actual_type is not None, f"new type definition does not have type {code}"
-        expected_type = mapping["external_name"]
-        assert (
-            actual_type == expected_type
-        ), f"expected: {expected_type}, got: {actual_type}"
+#     print(actual_types)
+
+#     for code, mapping in ID_TYPES_MAP.items():
+#         actual_type = actual_types.get(code)
+
+#         assert actual_type is not None, f"new type definition does not have type {code}"
+#         expected_type = mapping["external_name"]
+
+#         assert (
+#             actual_type == expected_type
+#         ), f"expected: {expected_type}, got: {actual_type}"
 
 
 def import_openbare_verlichting(in_file, type_file, out_file):
 
-    types = json.load(open(type_file))
-    check_types_as_expected(types)
+    # Discrepancy check between own type administration and source is disabled: Source is leading.
+    # To do: remove def check_types_as_expected when SIA has confirmed
+    # types = json.load(open(type_file))
+    # check_types_as_expected(types)
 
     data = json.load(open(in_file))
+    types = json.load(open(type_file))
+    generate_source_types_mapping(types)
 
-    geojson = json2geojson(data,)
+    geojson = json2geojson(data, types)
     output = open(out_file, "w")
 
     log.info("writing output...")
--- src/dags/openbare_verlichting.py
+++ src/dags/openbare_verlichting.py
@@ -1,31 +1,57 @@
+import operator
+
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
-from postgres_check_operator import PostgresCheckOperator
+
+
+from provenance_rename_operator import ProvenanceRenameOperator
+from postgres_rename_operator import PostgresTableRenameOperator
 from http_fetch_operator import HttpFetchOperator
 
+
 from common import (
-    vsd_default_args,
+    default_args,
     pg_params,
     slack_webhook_token,
     DATAPUNT_ENVIRONMENT,
     MessageOperator,
 )
 
-from common.sql import (
-    SQL_TABLE_RENAME,
-    SQL_CHECK_COUNT,
+from postgres_check_operator import (
+    PostgresMultiCheckOperator,
+    COUNT_CHECK,
+    GEO_CHECK,
 )
 
 from importscripts.import_openbare_verlichting import import_openbare_verlichting
 
 dag_id = "openbare_verlichting"
+variables = Variable.get("openbare_verlichting", deserialize_json=True)
+data_endpoints = variables["data_endpoints"]
+dataset_objects = list(data_endpoints.keys())[0]
+dataset_objecttypes = list(data_endpoints.keys())[1]
+tmp_dir = f"/tmp/{dag_id}"
+total_checks = []
+count_checks = []
+geo_checks = []
+check_name = {}
 
-with DAG(dag_id, default_args=vsd_default_args, template_searchpath=["/"]) as dag:
+# needed to put quotes on elements in geotypes for SQL_CHECK_GEO
+def quote(instr):
+    return f"'{instr}'"
 
-    tmp_dir = f"/tmp/{dag_id}"
 
+with DAG(
+    dag_id,
+    description="locatie, status en type van een openbare verlichting.",
+    default_args=default_args,
+    user_defined_filters=dict(quote=quote),
+    template_searchpath=["/"],
+) as dag:
+
+    # 1. Post message on slack
     slack_at_start = MessageOperator(
         task_id="slack_at_start",
         http_conn_id="slack",
@@ -34,61 +60,111 @@ with DAG(dag_id, default_args=vsd_default_args, template_searchpath=["/"]) as da
         username="admin",
     )
 
-    objects_fetch = HttpFetchOperator(
-        task_id="objects_fetch",
-        endpoint="{{ var.json.openbare_verlichting.objects_endpoint }}",
-        http_conn_id="verlichting_conn_id",
-        tmp_file=f"{tmp_dir}/objects-source.json",
-    )
+    # 2. Create temp directory to store files
+    mkdir = BashOperator(task_id="mkdir", bash_command=f"mkdir -p {tmp_dir}")
 
-    types_fetch = HttpFetchOperator(
-        task_id="types_fetch",
-        endpoint="{{ var.json.openbare_verlichting.types_endpoint }}",
-        http_conn_id="verlichting_conn_id",
-        tmp_file=f"{tmp_dir}/objects-types.json",
-    )
+    # 3. Download data
+    download_data = [
+        HttpFetchOperator(
+            task_id=f"download_{dataset}",
+            endpoint=f"{endpoint}",
+            http_conn_id="verlichting_conn_id",
+            output_type="text",
+            tmp_file=f"{tmp_dir}/{dataset}.json",
+        )
+        for dataset, endpoint in data_endpoints.items()
+    ]
 
-    import_data = PythonOperator(
-        task_id="import_data",
+    # 4. Convert json to Geojson
+    convert_to_geojson = PythonOperator(
+        task_id="convert_to_geojson",
         python_callable=import_openbare_verlichting,
         op_args=[
-            f"{tmp_dir}/objects-source.json",
-            f"{tmp_dir}/objects-types.json",
-            f"{tmp_dir}/objects.geo.json",
+            f"{tmp_dir}/{dataset_objects}.json",  # input: openbare verlichting objects (main data)
+            f"{tmp_dir}/{dataset_objecttypes}.json",  # input: openbare verlichting objecttypes (reference data)
+            f"{tmp_dir}/objects.geo.json",  # output: geojson
         ],
     )
 
-    extract_geojson = BashOperator(
-        task_id="extract_geojson",
+    # 5. Geojson to SQL
+    geojson_to_SQL = BashOperator(
+        task_id="geojson_to_SQL",
         bash_command=f"ogr2ogr --config PG_USE_COPY YES -f 'PGDump' "
-        f"-nln {dag_id}_new "
-        f"{tmp_dir}/objects.sql {tmp_dir}/objects.geo.json",
+        f"-nln {dag_id}_{dag_id}_new "
+        f"{tmp_dir}/objects.sql {tmp_dir}/objects.geo.json "
+        f"-lco GEOMETRY_NAME=geometry "
+        f"-lco FID=ID",
     )
 
+    # 6. Insert data into DB
     create_table = BashOperator(
         task_id="create_table",
         bash_command=f"psql {pg_params()} < {tmp_dir}/objects.sql",
     )
 
-    check_count = PostgresCheckOperator(
-        task_id="check_count",
-        sql=SQL_CHECK_COUNT,
-        params=dict(tablename=f"{dag_id}_new", mincount=129410),
+    # 7. Rename COLUMNS based on Provenance
+    provenance_translation = ProvenanceRenameOperator(
+        task_id="rename_columns",
+        dataset_name=f"{dag_id}",
+        prefix_table_name=f"{dag_id}_",
+        postfix_table_name="_new",
+        rename_indexes=False,
+        pg_schema="public",
     )
 
-    rename_table = PostgresOperator(
+    # PREPARE CHECKS
+    count_checks.append(
+        COUNT_CHECK.make_check(
+            check_id=f"count_check",
+            pass_value=25,
+            params=dict(table_name=f"{dag_id}_{dag_id}_new"),
+            result_checker=operator.ge,
+        )
+    )
+
+    geo_checks.append(
+        GEO_CHECK.make_check(
+            check_id=f"geo_check",
+            params=dict(table_name=f"{dag_id}_{dag_id}_new", geotype=["POINT"],),
+            pass_value=1,
+        )
+    )
+
+    total_checks = count_checks + geo_checks
+
+    # 8. RUN bundled CHECKS
+    multi_checks = PostgresMultiCheckOperator(
+        task_id=f"multi_check", checks=total_checks
+    )
+    # 9. Rename table
+    rename_table = PostgresTableRenameOperator(
         task_id="rename_table",
-        sql=SQL_TABLE_RENAME,
-        params=dict(tablename=f"{dag_id}"),
+        old_table_name=f"{dag_id}_{dag_id}_new",
+        new_table_name=f"{dag_id}_{dag_id}",
     )
 
-(
-    slack_at_start
-    >> objects_fetch
-    >> types_fetch
-    >> import_data
-    >> extract_geojson
-    >> create_table
-    >> check_count
-    >> rename_table
-)
+
+for data in zip(download_data):
+
+    data >> convert_to_geojson >> geojson_to_SQL >> create_table >> provenance_translation >> multi_checks >> rename_table
+
+slack_at_start >> mkdir >> download_data
+
+dag.doc_md = """
+    #### DAG summery
+    This DAG containts location, status and type of public lighting
+    #### Mission Critical
+    Classified as 2 (beschikbaarheid [range: 1,2,3])
+    #### On Failure Actions
+    Fix issues and rerun dag on working days
+    #### Point of Contact
+    Inform the businessowner at [businessowner]@amsterdam.nl
+    #### Business Use Case / process / origin
+    Na
+    #### Prerequisites/Dependencies/Resourcing
+    https://api.data.amsterdam.nl/v1/docs/datasets/openbare_verlichting.html
+    https://api.data.amsterdam.nl/v1/docs/wfs-datasets/openbare_verlichting.html
+    Example geosearch: 
+    https://api.data.amsterdam.nl/geosearch?datasets=openbare_verlichting/openbare_verlichting&x=111153&y=483288&radius=10
+    https://api.data.amsterdam.nl/geosearch?datasets=openbare_verlichting/openbare_verlichting&x=111153&y=483288&radius=10
+"""
