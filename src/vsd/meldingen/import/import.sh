be9476a5907594fd648c50d0fe14bd0a5dcbb182
Bart Heupers <bart.heupers@gmail.com>
2020-10-06 11:29:59 +0200

    Import meldingen statistiek

---
+++ src/vsd/meldingen/import/import.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -e
+source ${SHARED_DIR}/import/config.sh
+source ${SHARED_DIR}/import/before.sh
+
+ENVIRONMENT=${DATAPUNT_ENVIRONMENT:-acceptance}
+
+echo "Process import data"
+ZIPFILE=mora.zip
+OBJECTSTORE_PATH=Dataservices/meldingen/${ENVIRONMENT}/$ZIPFILE
+
+mkdir -p ${TMPDIR}/meldingen
+echo "Download file from objectstore"
+python $SHARED_DIR/utils/get_objectstore_file.py "$OBJECTSTORE_PATH" -s objectstore_dataservices
+
+unzip $TMPDIR/meldingen/${ENVIRONMENT}/$ZIPFILE -d ${TMPDIR}
+
+psql -X --set ON_ERROR_STOP=on << SQL
+DROP TABLE IF EXISTS pte.mora_melding_dag_buurt_categorie CASCADE;
+CREATE SCHEMA IF NOT EXISTS pte;
+\i ${TMPDIR}/mora.backup
+SQL
+
+psql -X --set ON_ERROR_STOP=on << SQL
+CREATE SEQUENCE IF NOT EXISTS public.meldingen_statistiek_seq;
+CREATE TABLE IF NOT EXISTS public.meldingen_statistiek (
+  id integer NOT NULL DEFAULT nextval('meldingen_statistiek_seq'),
+  datum_melding date,
+  buurtcode character varying,
+  buurtnaam character varying,
+  wijkcode character varying,
+  wijknaam character varying,
+  stadsdeelcode character varying,
+  stadsdeelnaam character varying,
+  categorie character varying,
+  subrubriek character varying,
+  aantal integer
+);
+BEGIN;
+TRUNCATE public.meldingen_statistiek;
+ALTER SEQUENCE meldingen_statistiek_seq RESTART WITH 1;
+INSERT INTO public.meldingen_statistiek(
+  datum_melding,
+  buurtcode,
+  buurtnaam,
+  wijkcode,
+  wijknaam,
+  stadsdeelcode,
+  stadsdeelnaam,
+  categorie,
+  subrubriek,
+  aantal)
+SELECT melding_datum_melding,
+       melding_buurt_code,
+       melding_buurt_naam,
+       melding_wijk_code,
+       melding_wijk_naam,
+       melding_stadsdeel_code,
+       melding_stadsdeel_naam,
+       melding_categorie,
+       melding_subrubriek,
+       melding_aantal
+FROM pte.mora_melding_dag_buurt_categorie;
+DROP TABLE pte.mora_melding_dag_buurt_categorie CASCADE;
+COMMIT;
+SQL
+
+
--- src/vsd/shared/utils/get_objectstore_file.py
+++ src/vsd/shared/utils/get_objectstore_file.py
@@ -9,7 +9,7 @@ from common.objectstore import fetch_objectstore_credentials
 log = logging.getLogger(__name__)
 
 
-def get_objectstore_file(location, dir1):
+def get_objectstore_file(location, dir1, connection):
     path = "/".join(location.split("/")[1:])
     output_path = dir1 + "/" + path
     if os.path.isfile(output_path):
@@ -17,7 +17,11 @@ def get_objectstore_file(location, dir1):
         return
     else:
         log.warning(f"Get file {output_path}")
-    connection = objectstore.get_connection(fetch_objectstore_credentials())
+    if connection:
+        credentials = fetch_objectstore_credentials(connection)
+    else:
+        credentials = fetch_objectstore_credentials(connection)
+    connection = objectstore.get_connection(credentials)
     container = location.split("/")[0]
     new_data = objectstore.get_object(connection, {"name": path}, container)
     output_dir = "/".join(output_path.split("/")[:-1])
@@ -30,6 +34,7 @@ def get_objectstore_file(location, dir1):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
+    parser.add_argument("-s", "--swift_connection")
     args = parser.parse_args()
     tmpdir = os.getenv("TMPDIR", "/tmp/")
-    get_objectstore_file(args.filename, tmpdir)
+    get_objectstore_file(args.filename, tmpdir, args.swift_connection)
