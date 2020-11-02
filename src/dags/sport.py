ca5175c1f2c0effae1829c45caf84054a4639ff9
Chris van Riel <c.van.riel@amsterdam.nl>
2020-11-02 22:31:26 +0100

    [bugfix] the sport source files are now correctly encoded, with the exception of one: Sportvelden. For now the non-encoded karakter is replaced by a forward slash. The to-do action is to contact the maintainer of the source file to ask for the encoding schema used

--- src/dags/sport.py
+++ src/dags/sport.py
@@ -1,4 +1,5 @@
-import operator, re, requests
+import operator
+import re
 
 from airflow import DAG
 from airflow.models import Variable
@@ -6,6 +7,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
+from ogr2ogr_operator import Ogr2OgrOperator
 
 from http_fetch_operator import HttpFetchOperator
 from provenance_rename_operator import ProvenanceRenameOperator
@@ -49,8 +51,11 @@ def clean_data(file_name):
     data = open(file_name, "r").read()
     remove_returns = re.sub(r"[\n\r]", "", data)
     remove_double_spaces = re.sub(r"[ ]{2,}", " ", remove_returns)
+    # TODO: contact data maintainer to ask for encoding schema for 
+    # sportvelden specificly, other datasets can be correctly encoded.
+    remove_unkown_karakter = re.sub(r"ï¿½", '/', remove_double_spaces)
     with open(file_name, "w") as output:
-        output.write(remove_double_spaces)
+        output.write(remove_unkown_karakter)
 
 with DAG(
     dag_id,
@@ -79,6 +84,7 @@ with DAG(
             http_conn_id="api_data_amsterdam_conn_id",
             tmp_file=f"{tmp_dir}/{file_name}.{re.sub('_.*', '', source_type)}",
             output_type="text",
+            encoding_schema=f"{'ISO-8859-1' if source_type == 'geojson_file' else 'UTF-8'}",
         )
         for source_type, data in files_to_download.items()
         for file_name, url in data.items()
@@ -86,7 +92,7 @@ with DAG(
 
     # 4. Download source 2: .geojson from Maps.amsterdam.nl
     download_data_maps = [ 
-    HttpFetchOperator(
+        HttpFetchOperator(
             task_id=f"download_maps_{file_name}_{source_type}",
             endpoint=f"{url}",
             http_conn_id="ams_maps_conn_id",
@@ -102,33 +108,15 @@ with DAG(
     Interface = DummyOperator(task_id="interface")
    
     # 6.create the SQL for creating the table using ORG2OGR PGDump
-    to_sql = [
-        BashOperator(
+    to_sql = [                 
+        Ogr2OgrOperator(
             task_id=f"{source_type}_to_SQL_{file_name}",
-            bash_command=f"ogr2ogr -f 'PGDump' "
-            f"-s_srs EPSG:4326 -t_srs EPSG:28992 "
-            f"-nln {dag_id}_{file_name}_new "
-            f"{tmp_dir}/{file_name}.sql {tmp_dir}/{file_name}.{re.sub('_.*', '', source_type)} "
-            f"-lco SEPARATOR=COMMA "
-            f"-oo AUTODETECT_TYPE=YES "
-            f"-lco FID=ID "
-            f"-lco GEOMETRY_NAME=geometry "
-            f"--config OGR_TRUNCATE YES "
-        )
+            target_table_name=f"{dag_id}_{file_name}_new",
+            sql_output_file=f"{tmp_dir}/{file_name}.sql",
+            input_file=f"{tmp_dir}/{file_name}.{re.sub('_.*', '', source_type)}",
+        )   
         for source_type, data in files_to_SQL.items()
         for file_name in data.keys()
-    ]
-
-    # 7. Convert SQL to store all karakters between the code points 128 - 255 (wich are different in iso-8859-1) in UTF-8
-    convert_to_UTF8 = [         
-        BashOperator(
-        task_id=f"convert_to_utf8_{file_name}",
-        bash_command=f"iconv -f iso-8859-1 -t utf-8  {tmp_dir}/{file_name}.sql > "
-        f"{tmp_dir}/{file_name}.sql.utf8.sql",
-    )
-    for _, data in files_to_SQL.items()
-    for file_name in data.keys()
-
     ]
 
     # 8. Clean data from returns and spaces
@@ -136,7 +124,7 @@ with DAG(
         PythonOperator(
             task_id=f"cleanse_{file_name}", 
             python_callable=clean_data,
-            op_args=[f"{tmp_dir}/{file_name}.sql.utf8.sql",]
+            op_args=[f"{tmp_dir}/{file_name}.sql",]
         )
         for _, data in files_to_SQL.items()
         for file_name in data.keys()
@@ -146,7 +134,7 @@ with DAG(
     load_tables = [
         BashOperator(
             task_id=f"load_table_{file_name}",
-            bash_command=f"psql {pg_params()} < {tmp_dir}/{file_name}.sql.utf8.sql",
+            bash_command=f"psql {pg_params()} < {tmp_dir}/{file_name}.sql",
         )
         for data in files_to_SQL.values()
         for file_name in data.keys()
@@ -252,9 +240,9 @@ with DAG(
 
         data_maps >> Interface >> to_sql
     
-    for (to_sql, convert, cleanse_data, load_tables) in zip(to_sql, convert_to_UTF8, cleanse_data, load_tables):
+    for (to_sql, cleanse_data, load_tables) in zip(to_sql, cleanse_data, load_tables):
 
-        to_sql >> convert >> cleanse_data >> load_tables >> Interface2 >> add_geom_col
+        to_sql >> cleanse_data >> load_tables >> Interface2 >> add_geom_col
             
     for add_geometry in add_geom_col:
 
