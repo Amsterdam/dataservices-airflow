575a41d97bf427ecd17a7dd64b4703801d9c65b4
cvriel <chrisvanriel@gmail.com>
2020-11-03 18:26:43 +0100

    Update sport.py

--- src/dags/sport.py
+++ src/dags/sport.py
@@ -53,7 +53,9 @@ def clean_data(file_name):
     remove_double_spaces = re.sub(r"[ ]{2,}", " ", remove_returns)
     # TODO: contact data maintainer to ask for encoding schema for 
     # sportvelden specificly, other datasets can be correctly encoded.
-    remove_unkown_karakter = re.sub(r"ï¿½", '/', remove_double_spaces)
+    # the unicode replacement karakter is translated to the EM DASH sign
+    # the source seems to have the replacement karakter in it without no possibity to transform it anymore
+    remove_unkown_karakter = re.sub(u'\uFFFD', u"\u2014", remove_double_spaces)
     with open(file_name, "w") as output:
         output.write(remove_unkown_karakter)
 
@@ -84,7 +86,7 @@ with DAG(
             http_conn_id="api_data_amsterdam_conn_id",
             tmp_file=f"{tmp_dir}/{file_name}.{re.sub('_.*', '', source_type)}",
             output_type="text",
-            encoding_schema=f"{'ISO-8859-1' if source_type == 'geojson_file' else 'UTF-8'}",
+            encoding_schema="UTF-8",
         )
         for source_type, data in files_to_download.items()
         for file_name, url in data.items()
@@ -119,7 +121,7 @@ with DAG(
         for file_name in data.keys()
     ]
 
-    # 8. Clean data from returns and spaces
+    # 7. Clean data from returns and spaces
     cleanse_data = [ 
         PythonOperator(
             task_id=f"cleanse_{file_name}", 
@@ -130,7 +132,7 @@ with DAG(
         for file_name in data.keys()
     ]
 
-    # 9. Load data into the table
+    # 8. Load data into the table
     load_tables = [
         BashOperator(
             task_id=f"load_table_{file_name}",
@@ -140,11 +142,11 @@ with DAG(
         for file_name in data.keys()
     ]
 
-    # 10. Dummy operator acts as an Interface between parallel tasks to another parallel tasks (i.e. lists or tuples) 
+    # 9. Dummy operator acts as an Interface between parallel tasks to another parallel tasks (i.e. lists or tuples) 
     #   with different number of lanes (without this intermediar, Airflow will give an error)
     Interface2 = DummyOperator(task_id="interface2")
 
-    # 11. Add geometry column for tables 
+    # 10. Add geometry column for tables 
     #    where source file (like the .csv files) has no geometry field, only x and y fields  
     add_geom_col = [ 
         PostgresOperator(
@@ -156,13 +158,13 @@ with DAG(
 
     ] 
 
-    # 12. delete duplicate rows in zwembad en sporthal, en gymzaal en sportzaal
+    # 11. delete duplicate rows in zwembad en sporthal, en gymzaal en sportzaal
     delete_duplicate_rows = PostgresOperator(
             task_id=f"del_duplicate_rows", 
             sql=DEL_ROWS,
         )
     
-    # 13. RENAME columns based on PROVENANCE    
+    # 12. RENAME columns based on PROVENANCE    
     provenance_translation = ProvenanceRenameOperator(
         task_id="provenance_rename",
         dataset_name=f"{dag_id}",
@@ -211,7 +213,7 @@ with DAG(
 
     
     
-    # 14. Execute bundled checks on database
+    # 13. Execute bundled checks on database
     multi_checks = [
         PostgresMultiCheckOperator(
             task_id=f"multi_check_{file_name}", checks=check_name[f"{file_name}"]
@@ -220,7 +222,7 @@ with DAG(
         for file_name in data.keys()
     ]
   
-    # 15. RENAME TABLES
+    # 14. RENAME TABLES
     rename_tables = [
         PostgresTableRenameOperator(
             task_id=f"rename_table_{file_name}",
