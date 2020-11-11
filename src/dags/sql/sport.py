3d38b033a49b09227d2f3342a2ee964e48e705ee
Chris van Riel <c.van.riel@amsterdam.nl>
2020-11-11 09:12:40 +0100

    [DS-507] data issues Sport fix

--- src/dags/sql/sport.py
+++ src/dags/sql/sport.py
@@ -3,6 +3,7 @@
 # Therefore the geometry column must be created (based upon the X and Y values)
 # Before creating the column geometry, there is a check if a X and Y column exists
 # Also een check is done on the existince of a value in X and Y (data may contain empty values)
+# Also a check is done on the presence of karakter values in X and Y (data may contain karakters!)
 # Finally the X and Y columns are dropped when the geometry column is created
 ADD_GEOMETRY_COL = """
     DO $$DECLARE counter_x_y integer; 
@@ -21,7 +22,8 @@ ADD_GEOMETRY_COL = """
         UPDATE {{ params.tablename }}_new  
         SET geometry = ST_Transform(ST_SetSRID(ST_MakePoint(REPLACE(x, ',', '.')::double precision, REPLACE(y, ',', '.')::double precision), 4326), 28992)
         WHERE 1=1
-        AND length(x) > 0 and length(y) > 0;   
+        AND (x !~* '[a-z]+' AND y !~* '[a-z]+')
+        AND (length(x) > 0 AND length(y) > 0);
         DROP INDEX IF EXISTS {{ params.tablename }}_new_geom_idx; 
         CREATE INDEX {{ params.tablename }}_new_geom_idx ON {{ params.tablename }}_new USING GIST (geometry);
 
