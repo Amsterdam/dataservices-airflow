446f75e4b34bbb2fc8360897dfb2797f44866315
Chris van Riel <c.van.riel@amsterdam.nl>
2020-11-26 13:44:31 +0100

    [DS-459] fix

--- src/plugins/pgcomparator_cdc_operator.py
+++ src/plugins/pgcomparator_cdc_operator.py
@@ -4,8 +4,8 @@ from airflow.models.baseoperator import BaseOperator
 
 
 class PgComparatorCDCOperator(BaseOperator):
-    """ 
-    PG_comparator CDC (change data capture) is used to detect modifications (insert, updates and deletes) on a target table based on a source table. 
+    """
+    PG_comparator CDC (change data capture) is used to detect modifications (insert, updates and deletes) on a target table based on a source table.
     Detected modifications are synchronzed into the target table.
     """
 
@@ -14,14 +14,14 @@ class PgComparatorCDCOperator(BaseOperator):
         self.source_table = source_table
         self.target_table = target_table
         self.db_conn_id = db_conn_id if db_conn_id else 'AIRFLOW_CONN_POSTGRES_DEFAULT'
-    
-    def execute(self, context=None):        
+
+    def execute(self, context=None):
         env = Env()
-        db_connection = env(self.db_conn_id).split("?")[0].split("//")[1]  
-        
+        db_connection = env(self.db_conn_id).split("?")[0].split("//")[1]
+
         self.log.info("Start change data capture (with pg_comparator) from: %s to: %s", self.source_table,  self.target_table)
         subprocess.run(
-            f'pg_comparator --verbose --do-it --synchronize --max-ratio=2.0 ' \
+            f'pg_comparator --do-it --synchronize --max-ratio=2.0 ' \
             f'pgsql://{db_connection}/{self.source_table} ' \
             f'pgsql://{db_connection}/{self.target_table}' \
             , shell=True, check=True
