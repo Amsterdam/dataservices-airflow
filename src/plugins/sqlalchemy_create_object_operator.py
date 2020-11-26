bb9c5f71d6020e38ac5541a16a145567285b55c3
Chris van Riel <c.van.riel@amsterdam.nl>
2020-11-26 14:58:34 +0100

    [DS-514] add indexoperator

---
+++ src/plugins/sqlalchemy_create_object_operator.py
@@ -0,0 +1,62 @@
+from airflow.models.baseoperator import BaseOperator
+
+from schematools.utils import schema_fetch_url_file
+from schematools.importer.base import BaseImporter
+from schematools.types import DatasetSchema, SchemaType
+from schematools.cli import _get_engine
+
+from environs import Env
+
+env = Env()
+# split is used to remove url params, if exists.
+# for database connection the url params must be omitted.
+default_db_conn = env("AIRFLOW_CONN_POSTGRES_DEFAULT").split("?")[0]
+
+
+class SqlAlchemyCreateObjectOperator(BaseOperator):
+    """ This operator takes a JSON data schema definition and DB connection to create the specified
+    tables and/or an index. The latter is based on the Identifier object in the data JSON schema i.e.
+    "identifier": ["identificatie", "volgnummer"] which is common for temporal data.
+    Note: The table creation alone leads to the creation of a PK which is already indexed by default.
+    Note2: A geometry column type, when specified in the JSON schema, also defaults to an index in the database.
+    """
+
+    def __init__(
+        self,
+        data_schema_name,
+        data_schema_env=None,
+        db_conn=None,
+        ind_table=None,
+        ind_identifier_index=None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.data_schema_name = data_schema_name
+        # Optionals
+        self.data_schema_env = f"{data_schema_env}." if data_schema_env else ""
+        self.db_conn = db_conn if db_conn else default_db_conn
+        self.ind_table = True if ind_table is None else ind_table
+        self.ind_identifier_index = (
+            True if ind_identifier_index is None else ind_identifier_index
+        )
+
+    def execute(self, context=None):
+        """ Executes the 'generate_db_object' method from schema-tools.
+        Which leads to the creation of tables and/or an index on the identifier (as specified in the data JSON schema).
+        By default both tables and the identifier index are created.
+        By setting the boolean indicators in the method parameters, tables or an identifier index (per table) can be created.
+        """
+        data_schema_url = f"https://{self.data_schema_env}schemas.data.amsterdam.nl/datasets/{self.data_schema_name}/{self.data_schema_name}"
+        data = schema_fetch_url_file(data_schema_url)
+        engine = _get_engine(self.db_conn)
+        parent_schema = SchemaType(data)
+        dataset_schema = DatasetSchema(parent_schema)
+        importer = BaseImporter(dataset_schema, engine)
+
+        for table in data["tables"]:
+            importer.generate_db_objects(
+                table["id"],
+                ind_tables=self.ind_table,
+                ind_identifier_index=self.ind_identifier_index,
+            )
