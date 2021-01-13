9405687622970fdd626dd430cd346036967fb219
Chris van Riel <c.van.riel@amsterdam.nl>
2021-01-13 16:29:24 +0100

    [DS-563] add permissions operator
    Added a permissions operator that uses permissions logic from schema-tools.
    Also added a permissions DAG to check if there are executed DAG's within a time window.
    
    Alternative would be to set the permissions as last step within each dag. That would be certainly possible but add an extra step to the dags. To avoid unnecessary DAG changes, a generic DAG could be the solutions (as choosen for now to implement).
    
    The permissions operator can cope with both scenario's: (1) a permissions dag or (2) as step for each individual dag.

---
+++ src/dags/airflow_db_permissions.py
@@ -0,0 +1,42 @@
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from postgres_permissions_operator import PostgresPermissionsOperator
+
+from common import (
+    default_args,
+    slack_webhook_token,
+    DATAPUNT_ENVIRONMENT,
+    MessageOperator,
+)
+
+dag_id = "airflow_db_permissions"
+logger = logging.getLogger(__name__)
+
+with DAG(
+    dag_id,
+    default_args=default_args,
+    schedule_interval="*/15 * * * *",
+    catchup=False,
+    description=f"set grants on database objects to database roles based upon schema auth definition, based upon successfully executed dags within specified time window.",
+) as dag:
+
+    # 1. Post info message on slack
+    slack_at_start = MessageOperator(
+        task_id="slack_at_start",
+        http_conn_id="slack",
+        webhook_token=slack_webhook_token,
+        message=f"Starting {dag_id} ({DATAPUNT_ENVIRONMENT})",
+        username="admin",
+    )
+
+    # 2. Add grants (in batch mode)
+    # seeks for dags that successfully executed within time window
+    # default time window is set on 30 min.
+    # beware: currently the logic assumes dag_id == dataset name
+    # TODO look for possibility to use other then dag_id as dataset name
+    grants = PostgresPermissionsOperator(task_id="grants", batch_ind=True)
+
+# FLOW
+slack_at_start >> grants
