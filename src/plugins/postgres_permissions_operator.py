1dc168e2ef209dbfc3db299511ef1d574f2baef9
Chris van Riel <c.van.riel@amsterdam.nl>
2021-01-13 16:29:24 +0100

    [DS-563] add permissions operator
    Added a permissions operator that uses permissions logic from schema-tools.
    Also added a permissions DAG to check if there are executed DAG's within a time window.
    
    Alternative would be to set the permissions as last step within each dag. That would be certainly possible but add an extra step to the dags. To avoid unnecessary DAG changes, a generic DAG could be the solutions (as choosen for now to implement).
    
    The permissions operator can cope with both scenario's: (1) a permissions dag or (2) as step for each individual dag.

---
+++ src/plugins/postgres_permissions_operator.py
@@ -0,0 +1,171 @@
+import logging
+import pendulum
+
+from datetime import timedelta
+from environs import Env
+
+from schematools.utils import schema_defs_from_url
+from schematools.permissions import apply_schema_and_profile_permissions
+from schematools.cli import _get_engine
+
+from airflow.models.baseoperator import BaseOperator
+from airflow.models.dagrun import DagRun
+from airflow.settings import Session
+
+env = Env()
+
+# split is used to remove url params, if exists. For database connection the url params must be omitted.
+default_db_conn = env("AIRFLOW_CONN_POSTGRES_DEFAULT").split("?")[0]
+default_schema_url = env("SCHEMA_URL")
+datapunt_evironment = env("DATAPUNT_ENVIRONMENT")
+
+
+class PostgresPermissionsOperator(BaseOperator):
+    """
+    This operator executes DB grants << on >> DB tables / fields << to >> DB roles.
+
+    It can be envoked based upon a single dataset or a list of datasets. It uses the dag_id to identify the dataset.
+
+    The DB roles are derived from the AUTH element as defined in the Amsterdam schema.
+    When no AUTH element is provided in the Amsterdam schema, it default to DB role SCOPE_OPENBAAR.
+    DB users can be assigned as member to the DB roles to get access to DB objects.
+
+    Caveat:
+    When objects are dropped in the DB, the grants to the roles are removed as well.
+    This results that grants must be (re)applied again to the roles.
+
+    An other way to deal with is, is to update tables based upon the pgcomparator_cdc_operator.py.
+    Which detects differences between source and target table, and upserts the target table if nessacery
+    in stead of dropping, recreating the target table and inserting the records on each run.
+    """
+
+    def __init__(
+        self,
+        batch_ind=False,
+        batch_timewindow="0:30",
+        dag_name=None,
+        db_conn=default_db_conn,
+        schema_url=default_schema_url,
+        db_schema="public",
+        profiles=None,
+        role="AUTO",
+        scope=None,
+        dry_run=False,
+        create_roles=True,
+        revoke=False,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.batch_ind = batch_ind
+        self.batch_timewindow = batch_timewindow
+        self.dataset_name = dag_name
+        self.db_conn = db_conn
+        self.schema_url = schema_url
+        self.db_schema = db_schema
+        self.profiles = profiles
+        self.role = role
+        self.scope = scope
+        self.dry_run = dry_run
+        self.create_roles = create_roles
+        self.revoke = revoke
+
+    def execute(self, context=None):
+        """Executes the 'apply_schema_and_profile_permissions' method from schema-tools to set the database permissions on objects to roles"""
+
+        # setup logger so output can be added to the Airflow logs
+        logger = logging.getLogger(__name__)
+
+        # setup database connection where the database objects are present
+        engine = _get_engine(self.db_conn)
+
+        # Option ONE: batch grant
+        if self.batch_ind:
+
+            # get current datetime and make it aware of the TZ (mandatory)
+            now = pendulum.now("Europe/Amsterdam")
+
+            # calculate the delta between current datetime and specified time window
+            time_window_hour = int(self.batch_timewindow.split(':')[0])
+            time_window_minutes = int(self.batch_timewindow.split(':')[1])
+            delta = now - timedelta(hours=time_window_hour, minutes=time_window_minutes)
+
+            logger.info(f"the time window is set starting at: {delta} till now")
+
+            # setup an Airflow session to access Airflow repository data
+            session = Session()
+
+            # get list of dags that meet time window and state outcome
+            # it uses the Airflow DagRun class to get data
+            executed_dags_after_delta = [
+                dag.dag_id
+                for dag in session.query(DagRun)
+                .filter(DagRun.execution_date > delta)
+                .filter(DagRun._state == "success")
+                # exclude the dag itself that calls this batch grant method
+                .filter((DagRun.dag_id != "airflow_db_permissions"))
+            ]
+
+
+            if executed_dags_after_delta:
+
+                for dataset_name in executed_dags_after_delta:
+
+                    logger.info(
+                            f"set grants for {dataset_name}"
+                        )
+
+                    try:
+                        ams_schema = schema_defs_from_url(
+                            schemas_url=self.schema_url,
+                            dataset_name=dataset_name,
+                        )
+
+                        apply_schema_and_profile_permissions(
+                            engine=engine,
+                            pg_schema=self.db_schema,
+                            ams_schema=ams_schema,
+                            profiles=self.profiles,
+                            role=self.role,
+                            scope=self.scope,
+                            dry_run=self.dry_run,
+                            create_roles=self.create_roles,
+                            revoke=self.revoke,
+                        )
+
+                    except:
+                        logger.error(
+                            f"Could not get data schema for {dataset_name}"
+                        )
+                        continue
+
+            else:
+                logger.error(
+                    f"Nothing to grant, no finished dags detected within time window of {self.batch_timewindow}."
+                )
+
+        # Option TWO: grant on single dataset (can be used as a final step within a dag run)
+        elif self.dataset_name and not self.batch_ind:
+
+            try:
+                ams_schema = schema_defs_from_url(
+                    schemas_url=self.schema_url, dataset_name=self.dataset_name
+                )
+
+                apply_schema_and_profile_permissions(
+                    engine=engine,
+                    pg_schema=self.db_schema,
+                    ams_schema=ams_schema,
+                    profiles=self.profiles,
+                    role=self.role,
+                    scope=self.scope,
+                    dry_run=self.dry_run,
+                    create_roles=self.create_roles,
+                    revoke=self.revoke,
+                )
+
+            except:
+                logger.error(
+                    f"Could not get data schema for {self.dataset_name}"
+                )
+                pass
