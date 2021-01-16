a1144b83f425962643c7be560ac40e128949f769
cvriel <chrisvanriel@gmail.com>
2021-01-16 13:07:23 +0100

    [FIX] check number of records
    The number of events in the source are lowered, therefore the threshold check on the number of records is lowered as well

--- src/dags/evenementen.py
+++ src/dags/evenementen.py
@@ -122,7 +122,7 @@ with DAG(
     count_checks.append(
         COUNT_CHECK.make_check(
             check_id=f"count_check",
-            pass_value=75,
+            pass_value=25,
             params=dict(table_name=f"{dag_id}_{dag_id}_new"),
             result_checker=operator.ge,
         )
