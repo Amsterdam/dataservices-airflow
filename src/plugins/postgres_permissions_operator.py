daf402bc9abc345687e6073d0f3e507a802c00c3
cvriel <chrisvanriel@gmail.com>
2021-01-18 12:40:42 +0100

    [DS-563] typo removed space

--- src/plugins/postgres_permissions_operator.py
+++ src/plugins/postgres_permissions_operator.py
@@ -126,7 +126,7 @@ class PostgresPermissionsOperator(BaseOperator):
                 # exclude the dag itself that calls this batch grant method
                 .filter((DagRun.dag_id != "airflow_db_permissions"))
                 # exclude the update_dag, it does not contain DB objects to grant
-                .filter((DagRun.dag_id != "update_dags "))
+                .filter((DagRun.dag_id != "update_dags"))
             ]
 
             if executed_dags_after_delta:
