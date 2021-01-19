c8b51b02f60e5e8c6c175e1572b9adb9624d31a1
Bart Heupers <bart.heupers@gmail.com>
2021-01-19 20:09:58 +0100

    Fix missing AIRFLOW_CONN_SWIFT_DEFAULT from Openstack

--- src/dags/common/objectstore.py
+++ src/dags/common/objectstore.py
@@ -1,10 +1,14 @@
 import dsnparse
-from . import env
+import os
 
 
 def fetch_objectstore_credentials(swift_conn_id="swift_default"):
 
     env_varname = f"AIRFLOW_CONN_{swift_conn_id.upper()}"
+    from . import env
+    if not env(env_varname) and env_varname == "AIRFLOW_CONN_SWIFT_DEFAULT":
+        os.environ["AIRFLOW_CONN_SWIFT_DEFAULT"] = \
+            f"s3://{env('OS_USERNAME')}:{env('OS_PASSWORD')}@{env('OS_TENANT_NAME')}"
     airflow_conn_info = dsnparse.parse_environ(env_varname)
     return {
         "VERSION": "2.0",
