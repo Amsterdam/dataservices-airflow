ceb5ac2961018b1803df2334b255aecced4ac0a4
Bart Heupers <bart.heupers@gmail.com>
2021-01-20 10:17:08 +0100

    Better way to check existence of AIRFLOW_CONN_SWIFT_DEFAULT env variable

--- src/dags/common/objectstore.py
+++ src/dags/common/objectstore.py
@@ -3,19 +3,28 @@ import os
 
 
 def fetch_objectstore_credentials(swift_conn_id="swift_default"):
-
     env_varname = f"AIRFLOW_CONN_{swift_conn_id.upper()}"
     from . import env
-    if not env(env_varname) and env_varname == "AIRFLOW_CONN_SWIFT_DEFAULT":
-        os.environ["AIRFLOW_CONN_SWIFT_DEFAULT"] = \
-            f"s3://{env('OS_USERNAME')}:{env('OS_PASSWORD')}@{env('OS_TENANT_NAME')}"
-    airflow_conn_info = dsnparse.parse_environ(env_varname)
-    return {
+    result = {
         "VERSION": "2.0",
         "AUTHURL": env("OS_AUTH_URL"),
-        "TENANT_NAME": airflow_conn_info.host,
-        "TENANT_ID": airflow_conn_info.host,
-        "USER": airflow_conn_info.username,
-        "PASSWORD": airflow_conn_info.password,
         "REGION_NAME": "NL",
     }
+    if env_varname not in os.environ and env_varname == "AIRFLOW_CONN_SWIFT_DEFAULT":
+        result.update(
+            {
+                "TENANT_NAME": env('OS_TENANT_NAME'),
+                "TENANT_ID": env('OS_TENANT_NAME'),
+                "USER": env('OS_USERNAME'),
+                "PASSWORD": env('OS_PASSWORD'),
+            })
+    else:
+        airflow_conn_info = dsnparse.parse_environ(env_varname)
+        result.update(
+            {
+                "TENANT_NAME": airflow_conn_info.host,
+                "TENANT_ID": airflow_conn_info.host,
+                "USER": airflow_conn_info.username,
+                "PASSWORD": airflow_conn_info.password,
+            })
+    return result
