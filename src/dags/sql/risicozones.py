11aea9819b5500980d637b3390ba2b4dd4f4cf81
Chris van Riel <c.van.riel@amsterdam.nl>
2021-01-31 12:06:06 +0100

    [DS-389] plural table names

--- src/dags/sql/risicozones.py
+++ src/dags/sql/risicozones.py
@@ -1,12 +1,12 @@
 # Because geometry is ignored when importing .csv with ogr2ogr, it's set explicitly
 SET_GEOM = """
-    {% if 'lpgtankstation' in params.tablename or 'bedrijven' in params.tablename or 'lpgvulpunt' in params.tablename or 'lpgafleverzuil' in params.tablename  %}
+    {% if 'lpgtankstations' in params.tablename or 'bedrijven' in params.tablename or 'lpgvulpunten' in params.tablename or 'lpgafleverzuilen' in params.tablename  %}
     ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie_punt TYPE geometry(POINT, 0) using ST_GeomFromText(CASE WHEN geometrie_punt LIKE 'POINT%' THEN geometrie_punt ELSE NULL END);
     ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie_punt TYPE geometry(POINT, 28992) USING ST_SetSRID(geometrie_punt, 28992);
     CREATE INDEX {{ params.tablename }}_geom_punt_idx ON {{ params.tablename }} USING GiST (geometrie_punt);
     {% endif %}
 
-    {% if 'aardgasleiding' in params.tablename %}
+    {% if 'aardgasleidingen' in params.tablename %}
     ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometry(MULTILINESTRING, 0) using ST_GeomFromText(CASE WHEN geometrie LIKE 'MULTILINESTRING%' THEN geometrie ELSE NULL END);
     ALTER TABLE {{ params.tablename }} ALTER COLUMN geometrie TYPE geometry(MULTILINESTRING, 28992) USING ST_SetSRID(geometrie, 28992);
 
