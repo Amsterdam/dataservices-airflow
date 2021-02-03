596ce2000b83a63f5cf0a6804d67bab667c9d696
Chris van Riel <c.van.riel@amsterdam.nl>
2021-02-03 19:00:10 +0100

    [DS-389] peer review adjustements

--- src/dags/importscripts/import_risicozones.py
+++ src/dags/importscripts/import_risicozones.py
@@ -4,6 +4,7 @@ import sys
 import hashlib
 from collections import namedtuple
 from collections import defaultdict
+import operator
 from shapely import wkt
 from shapely.geometry import (
     GeometryCollection,
@@ -32,19 +33,11 @@ def get_header(file: str, extra_fields_to_add: list = None):
     """retreive header from file"""
     with open(file) as data:
         reader = csv.reader(data, dialect=get_dialect(file))
-        # reader = csv.reader(data, delimiter="|")
         header = list(map(lambda s: s.lower(), next(reader)))
         if extra_fields_to_add:
             for col in extra_fields_to_add:
                 header.append(col)
         return header
-
-
-def create_named_tuple(file: str, class_name: str, header: list = None):
-    """create named tuple based upon header file"""
-    if not header:
-        header = get_header(file)
-    return namedtuple(class_name, header)
 
 
 def file_parser(file: str):
@@ -53,13 +46,11 @@ def file_parser(file: str):
         yield from csv.reader(data, dialect=get_dialect(file))
 
 
-def file_iter(file: str, name_named_tuple: str, header: list = None):
+def file_iter(file: str):
     """retrieve data of file into named tuple"""
-    named_tuple = create_named_tuple(file, name_named_tuple, header)
     reader = file_parser(file)
     next(reader)  # skip header, just get data
-    for row in reader:
-        yield named_tuple(*row)
+    yield from reader
 
 
 def filter_rows(row, filter: dict = None):
@@ -81,15 +72,21 @@ def filter_rows(row, filter: dict = None):
     filter_type = FILTER_OPTIONS[filter["filter_type"].upper()]
 
     if filter_type == "startswith":
-        if eval(
-            f"'{getattr(row, filter['filter_column'])}'.{filter_type}('{filter['filter_value']}')"
-        ):
+        if row.startswith(filter['filter_value']):
             return True
+
+    if filter_type == "in":
+        if operator.contains(row, filter['filter_value'].upper()):
+            return True
+
+    if filter_type == "==":
+        if operator.eq(row, filter['filter_value'].upper()):
+            return True
+
+    # TODO: add more filter possibilities
     else:
-        if eval(
-            f"'{filter['filter_value'].upper()}' {filter_type} '{getattr(row, filter['filter_column'])}'"
-        ):
-            return True
+        return False
+
 
 
 def unique_row_id(values: list):
@@ -116,50 +113,62 @@ def save_file(output_file: str, header: list, rows: list):
 
 def merge_files_parser(
     file_target: str,
-    name_target: str,
     file_source: str,
-    name_source: str,
     name_join_key: str,
+    header: list,
     source_fields_names: list,
     target_fields_names: list,
     source_filter: dict = None,
     target_filter: dict = None,
 ):
     """generate all data from target file and extend it with selected data from source file"""
-    target_file = file_iter(file_target, name_target)
-    source_file = file_iter(file_source, name_source)
-    merge_header = get_header(file_target, target_fields_names)
-    merge_named_tuple = create_named_tuple(file_target, name_target, merge_header)
+    target_file = file_iter(file_target)
+    source_file = file_iter(file_source)
+    header_source = get_header(file_source)
+    header_target = get_header(file_target)
+    idx_key = header_target.index(name_join_key)
 
     # retrieve all relevant data from source file, to append to target file
     # used defaultdict to group element on one key (== name_join_key)
     source_data_to_merge = defaultdict(list)
     for row in source_file:
-        if filter_rows(row, source_filter):
+
+        if source_filter:
+            idx_col_to_filter = header_source.index(source_filter['filter_column'])
+            row_col = row[idx_col_to_filter]
+        else:
+            row_col = row
+
+        if filter_rows(row_col, source_filter):
             for field in source_fields_names:
                 # To avoid the number of values exceeds the number of columns to accomodate these values.
                 # This happens when the value of name_join_key is found more then once in the same file
                 # i.e. dmb_lpg_afleverzuil.csv
-                if len(source_data_to_merge[getattr(row, name_join_key)]) >= len(
+                if len(source_data_to_merge[row[idx_key]]) >= len(
                     source_fields_names
                 ):
                     continue
-
-                source_data_to_merge[getattr(row, name_join_key)].append(getattr(row, field, None))
+                idx_field = header_source.index(field)
+                source_data_to_merge[row[idx_key]].append(row[idx_field])
 
     # append data from source file to target file based upon mutual key
     for row in target_file:
-        if filter_rows(row, target_filter):
-            if source_data_to_merge[getattr(row, name_join_key)]:
-                yield merge_named_tuple(
-                    *row + tuple(source_data_to_merge[getattr(row, name_join_key)])
-                )
+
+        if target_filter:
+            idx_col_to_filter = header_target.index(target_filter['filter_column'])
+            row_col = row[idx_col_to_filter]
+        else:
+            row_col = row
+
+        if filter_rows(row_col, target_filter):
+            if source_data_to_merge[row[idx_key]]:
+                for value in source_data_to_merge[row[idx_key]]:
+                    row.append(value)
+                    yield row
 
 
 def merge_files_iter(
-    target_file_name: str,
     target_file: str,
-    source_file_name: str,
     source_file: str,
     mutual_key: str,
     map_source_field_to_target: dict,
@@ -177,10 +186,9 @@ def merge_files_iter(
         list(i)
         for i in merge_files_parser(
             target_file,
-            target_file_name,
             source_file,
-            source_file_name,
             mutual_key,
+            header,
             source_fields_names,
             target_fields_names,
             source_filter,
@@ -198,43 +206,32 @@ def merge_files_iter(
 
 def union_files_parser(
     target_file: str,
-    target_name: str,
     source_file: list,
     source_file_content_type: list,
     source_file_content_column: list,
     source_file_dir_path: str,
     id_column: str,
-    header: list,
     row_unique_cols: list,
 ):
     """generate all data from source files and concat it's content in one target file """
-    if source_file_content_type:
-        # add extra field to header, based on source_file_content_column, to store content type
-        target = create_named_tuple(
-            f"{source_file_dir_path}/{source_file[0]}",
-            target_name,
-            get_header(f"{source_file_dir_path}/{source_file[0]}", source_file_content_column),
-        )
-
-    else:
-        target = create_named_tuple(f"{source_file_dir_path}/{source_file[0]}", target_name)
-
+    header = get_header(f"{source_file_dir_path}/{source_file[0]}", source_file_content_column)
     # with a union, to avoid dubplicate key errors,
     # it is needed to create an unique value for the record identifcation field
     unique_row_value = []
     row_unique_cols = row_unique_cols if row_unique_cols else [id_column]
-    for file, type_ in zip(source_file, source_file_content_type):
-        reader = file_iter(f"{source_file_dir_path}/{file}", "source")
+    for file, content_type in zip(source_file, source_file_content_type):
+        reader = file_iter(f"{source_file_dir_path}/{file}")
         for row in reader:
             for col in row_unique_cols:
-                unique_row_value.append(getattr(row, col))
+                idx = header.index(col)
+                unique_row_value.append(row[idx])
             row_id = unique_row_id(unique_row_value)
-            row = eval(f"row._replace({id_column}='{row_id}')")
-            yield target(*row + tuple(type_.split()))
-
+            # set id column with unique value
+            row[0] = row_id
+            row.append(content_type)
+            yield row
 
 def union_files_iter(
-    target_name: str,
     target_file: str,
     source_file: tuple,
     source_file_dir_path: str,
@@ -252,13 +249,11 @@ def union_files_iter(
         list(i)
         for i in union_files_parser(
             target_file,
-            target_name,
             source_file,
             source_file_content_type,
             source_file_content_column,
             source_file_dir_path,
             id_column,
-            header,
             row_unique_cols,
         )
     ]
@@ -270,9 +265,7 @@ def union_files_iter(
 # Cleansing data logic
 # ----------------------------#
 
-
 def cleanse_misformed_data_parser(
-    source_name: str,
     source_file: str,
     header: list,
     row_unique_cols: list,
@@ -283,21 +276,20 @@ def cleanse_misformed_data_parser(
     generate cleaned up data from file
     For instance: header in camelcase, no unique key present, etc.
     """
-    data = file_iter(source_file, source_name, header)
-    target = create_named_tuple(source_file, source_name, header)
+    data = file_iter(source_file)
 
     for row in data:
         unique_row_value = []
         for col in row_unique_cols:
-            unique_row_value.append(getattr(row, col))
+            idx = header.index(col)
+            unique_row_value.append(row[idx])
         # setup unique key by hashing values of unique cols combination
         row_id = unique_row_id(unique_row_value)
-        row = eval(f"row._replace({id_column}='{row_id}')")
-        yield target(*row)
+        row[0] = row_id
+        yield row
 
 
 def cleanse_misformed_data_iter(
-    source_name: str,
     source_file: str,
     row_unique_cols: list,
     id_column: str = "id",
@@ -310,7 +302,7 @@ def cleanse_misformed_data_iter(
     rows = [
         list(i)
         for i in cleanse_misformed_data_parser(
-            source_name, source_file, header, row_unique_cols, id_column, output_file
+            source_file, header, row_unique_cols, id_column, output_file
         )
     ]
     output_file = output_file if output_file else source_file
@@ -323,7 +315,6 @@ def cleanse_misformed_data_iter(
 
 
 def unify_geometry_data_parser(
-    source_name: str,
     source_file: str,
     geom_data_type_to_use: str,
     geom_column: str = "geometrie",
@@ -331,9 +322,10 @@ def unify_geometry_data_parser(
     """generate translated geometry data.
     It translates GeometryCollection or single geom datatype to prefered multi geom datatype i.e. LineString to MultiLineString
     """
-
-    for row in file_iter(source_file, source_name):
-        geom = wkt.loads(getattr(row, geom_column))
+    header = get_header(source_file)
+    for row in file_iter(source_file):
+        geom_idx = header.index(geom_column)
+        geom = wkt.loads(row[geom_idx])
         result = None
         elements = []
         # geom is a collection; break it up
@@ -352,7 +344,7 @@ def unify_geometry_data_parser(
 
                 result = MultiPolygon(elements)
 
-            row = eval(f"row._replace({geom_column}='{result}')")
+            row[geom_idx] = result
 
         # geom is not a collection
         elif not isinstance(geom, eval(geom_data_type_to_use)):
@@ -364,17 +356,16 @@ def unify_geometry_data_parser(
                 elements.append(geom)
                 result = MultiPolygon(elements)
 
-            row = eval(f"row._replace({geom_column}='{result}')")
+            row[geom_idx] = result
 
         # geom type equals the geom_data_type_to_use
         else:
-            row = eval(f"row._replace({geom_column}='{row.geometrie}')")
+            continue
 
         yield row
 
 
 def unify_geometry_data_iter(
-    source_name: str,
     source_file: str,
     geom_data_type_to_use: str,
     geom_column: str = "geometrie",
@@ -386,7 +377,7 @@ def unify_geometry_data_iter(
     rows = [
         row
         for row in unify_geometry_data_parser(
-            source_name, source_file, geom_data_type_to_use, geom_column
+            source_file, geom_data_type_to_use, geom_column
         )
     ]
     output_file = output_file if output_file else source_file
