bc160a7f4c1e89e02df0ce548cdb7a20c41d17c5
Guido Kollerie <guido@kollerie.com>
2021-02-05 15:43:22 +0100

    SqlAlchemyCreateObjectOperator: schema url bug, regex table spec. [DS-559]
    A lot code in this project relies on environment variables for
    configuration. One of the environment variables used is `SCHEMA_URL`.
    Unfortunately this custom Airflow operator had the schema url largely
    hard coded; only the sub domain was configurable. The hard coded schema
    url was ripped out in favor for the environment variable `SCHEMA_URL`.
    
    In addition this operator could only process one table at a time.
    Creating PostgreSQL objects for multiple tables required invoking the
    operator once for each table. Given the way Airflow processes tasks this
    was a massive waste of time. By allowing a regex for the table specification
    we can retain the old behavior while opening up the ability to specify
    multiple tables at once. The table spec now has a default option that
    matches all tables in a given schema.
    
    - Removed all print statements in favor of logging statements.
    - Updated docstrings where applicable.
    - Added type annotations where applicable.

--- src/plugins/sqlalchemy_create_object_operator.py
+++ src/plugins/sqlalchemy_create_object_operator.py
@@ -1,68 +1,104 @@
-from airflow.models.baseoperator import BaseOperator
+import re
+from re import Pattern
+from typing import Union
 
-from schematools.utils import schema_fetch_url_file
+from airflow.models.baseoperator import BaseOperator
+from environs import Env
+from schematools.cli import _get_engine
 from schematools.importer.base import BaseImporter
 from schematools.types import DatasetSchema, SchemaType
-from schematools.cli import _get_engine
-
-from environs import Env
+from schematools.utils import schema_fetch_url_file
 
 env = Env()
 # split is used to remove url params, if exists.
 # for database connection the url params must be omitted.
-default_db_conn = env("AIRFLOW_CONN_POSTGRES_DEFAULT").split("?")[0]
+DEFAULT_DB_CONN = env("AIRFLOW_CONN_POSTGRES_DEFAULT").split("?")[0]
 SCHEMA_URL = env("SCHEMA_URL")
 
+
 class SqlAlchemyCreateObjectOperator(BaseOperator):
-    """This operator takes a JSON data schema definition and DB connection to create the specified
-    tables and/or an index. The latter is based on the Identifier object in the data JSON schema i.e.
-    "identifier": ["identificatie", "volgnummer"] which is common for temporal data. And on the Relation
-    specification in the schema where "many-to-many" intersection tables can be defined.
-    Note: The table creation alone leads to the creation of a PK which is already indexed by default.
-    Note2: A geometry column type, when specified in the JSON schema, also defaults to an index in the database.
+    """Create PostgreSQL objects based on an Amsterdam schema using SQLAlchemy
+
+    This operator takes a JSON data schema definition and DB connection to create the specified
+    tables and/or an index. The latter is based on the Identifier object in the data JSON schema
+    i.e. "identifier": ["identificatie", "volgnummer"] which is common for temporal data. And on
+    the Relation specification in the schema where "many-to-many" intersection tables can be
+    defined. Note: The table creation alone leads to the creation of a PK which is already
+    indexed by default. Note2: A geometry column type, when specified in the JSON schema,
+    also defaults to an index in the database.
     """
 
     def __init__(
         self,
-        data_schema_name,
-        data_table_name,
-        data_schema_env=None,
-        db_conn=default_db_conn,
-        ind_table=True,
-        ind_extra_index=True,
+        data_schema_name: str,
+        data_table_name: Union[str, Pattern] = re.compile(r".*"),
+        db_conn: str = DEFAULT_DB_CONN,
+        ind_table: bool = True,
+        ind_extra_index: bool = True,
         *args,
         **kwargs,
     ):
+        """Initialize SqlAlchemyCreateObjectOperator.
+
+        Args:
+            data_schema_name: Name of the schema to derive PostgreSQL objects from.
+            data_table_name: Table(s) to create PostgreSQL objects for. This can either be a
+                string of the format ``<prefix>_<scheme_table_name>``. Where ``prefix`` generally
+                is the dataset name/abbreviation and ``schema_table_name`` the name of the table
+                as specified in the schema. Or it can be a compiled regular expression. The latter
+                is mostly useful if you want to create PostgreSQL objects for all tables in a
+                given schema. This is also the default value (all tables).
+            db_conn: DB connection URL.
+            ind_table: Whether to creates indices (as specified in the schema).
+            ind_extra_index: Whether to created additional indices.
+            *args:
+            **kwargs:
+        """
         super().__init__(*args, **kwargs)
         self.data_schema_name = data_schema_name
-        self.data_table_name = data_table_name
-        # Optionals
-        self.data_schema_env = f"{data_schema_env}." if data_schema_env else ""
+        if isinstance(data_table_name, str):
+            self.data_table_name = re.compile(data_table_name)
+        else:
+            self.data_table_name = data_table_name
+
         self.db_conn = db_conn
         self.ind_table = ind_table
         self.ind_extra_index = ind_extra_index
 
     def execute(self, context=None):
-        """Executes the 'generate_db_object' method from schema-tools.
-        Which leads to the creation of tables and/or an index on the identifier (as specified in the data JSON schema).
-        By default both tables and the identifier and 'many-to-many table' indexes are created.
-        By setting the boolean indicators in the method parameters, tables or an identifier index (per table) can be created.
+        """Executes the ``generate_db_object`` method from schema-tools.
+
+        Which leads to the creation of tables and/or an index on the identifier (as specified in
+        the data JSON schema). By default both tables and the identifier and 'many-to-many
+        table' indexes are created. By setting the boolean indicators in the method parameters,
+        tables or an identifier index (per table) can be created.
         """
-        data_schema_url = f"{SCHEMA_URL.split('//')[0]}//{self.data_schema_env}{SCHEMA_URL.split('//')[1]}{self.data_schema_name}/{self.data_schema_name}"
+        data_schema_url = "/".join(
+            map(lambda s: s.strip("/"), (SCHEMA_URL, self.data_schema_name, self.data_schema_name))
+        )
         data = schema_fetch_url_file(data_schema_url)
         engine = _get_engine(self.db_conn)
         parent_schema = SchemaType(data)
         dataset_schema = DatasetSchema(parent_schema)
         importer = BaseImporter(dataset_schema, engine)
+        self.log.info(
+            "data_schema_url='%s', engine='%s', ind_table='%s', ind_extra_index='%s'.",
+            data_schema_url,
+            engine,
+            self.ind_table,
+            self.ind_extra_index,
+        )
+
         for table in data["tables"]:
-            if (
-                self.data_schema_name + "_" + table["id"]
-                == f"{self.data_table_name}"
-            ):
+            self.log.info("Considering table '%s'.", table["id"])
+            cur_table = f"{self.data_schema_name}_{table['id']}"
+            if re.fullmatch(self.data_table_name, cur_table):
+                self.log.info("Generating PostgreSQL objects for table '%s'.", table["id"])
                 importer.generate_db_objects(
                     table["id"],
                     ind_tables=self.ind_table,
                     ind_extra_index=self.ind_extra_index,
                 )
             else:
+                self.log.info("Skipping table '%s' (reason: no match).", table["id"])
                 continue
