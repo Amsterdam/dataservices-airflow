c36994edf2fcd12f7a64ac94dd218080e2605d2e
Guido Kollerie <guido@kollerie.com>
2021-02-12 15:14:28 +0100

    Reworked BBGA DAG to support `pg_comparator`'s `--pg-copy`. [DS-559]
    BBGA's largest table has over 6 million rows in it. The primary key of
    that table is of type `VARCHAR`. As it turns out `pg_comparator` is not
    able to use its much faster `--pg-copy` method on keys (primary or
    alternate) of that type. It requires them to be of type integer.
    
    To solve that problem we create yet another column named `cdc_id` of the
    largest integer type supported by `pg_comparator`. In it we store an 8
    byte hash (using BLAKE2s) of the original primary column. Next we invoke
    the reworked `PgComparatorCDCOperator` to use that column with the
    `use_pg_copy` option.
    
    The performance improvement is massive. In an Bhyve virtual machine
    running Alphine Linux, running the database container, the original
    PgComparatorCDCOperator initial run took 1hr26min. With the above
    optimizations the initial run only takes 6mins. That's 14x faster!!!
    
    Thanks to @cvriel for pointing out the existance of the `--pg-copy`
    option.

--- src/dags/bbga.py
+++ src/dags/bbga.py
@@ -1,11 +1,14 @@
 import csv
+import hashlib
 import itertools
 import shutil
+import sys
 import tempfile
 from csv import Dialect
+from dataclasses import dataclass
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Dict, Tuple, TypedDict
+from typing import Callable, Dict, Tuple, TypedDict, Type, Any
 
 from airflow import DAG
 from airflow.models import Variable
@@ -44,13 +47,76 @@ class bbga_dialect(Dialect):
     quoting = csv.QUOTE_NONE
 
 
+def int_hash(data: str) -> int:
+    """Calculate an 8 byte BLAKE2s digest/hash as a number.
+
+    .. warning:: By default BLAKE2s has a digest size of 32 bytes. Though PostgreSQL can
+       easily store numbers with that kind of precision (eg ``NUMERIC(78)`` in this particular
+       case) that would no longer be a number that ``pg_comparator`` considers a 'simple integer'.
+
+       Hence we have chosen to generate an 8 byte BLAKE2s digest. That digest can still be
+       stored in PostgreSQL's largest 'simple integer', namely the ``BIGINT``. However by making
+       the digest size 4 times smaller than default we do increase the chance of collisions.
+
+       I'm not yet sure if that is a wise decision. Should we get a collision within our
+       dataset PostgreSQL will notify us with a uniqueness constraint error on the column that
+       this function is used for. We won't get incorrect results, but things will break.
+
+    Args:
+        data: String to calculate the hash from.
+
+    Returns:
+        A integer representing the digest/hash.
+    """
+    return int.from_bytes(
+        # BIGINT is an 8 byte signed integer. Hence our digest should be signed as well.
+        hashlib.blake2s(data.encode(), digest_size=8).digest(),
+        sys.byteorder,
+        signed=True,
+    )
+
+
+def concat_columns(*column_values: object) -> str:
+    """Concatenate column values into "|" separated string.
+
+    Args:
+        *column_values: any number of column values as long as they can each be casted
+            to a ``str``.
+
+    Returns:
+        "|" separated string of ``column_values``.
+    """
+    return "|".join(map(str, column_values))
+
+
+def hash_columns(*column_values: object) -> int:
+    """Hash column values into an integer hash value.
+
+    Args:
+        *column_values: any number of column values as long as they can each be casted
+            to a ``str``.
+
+    Returns:
+        A number representing the hash value of the ``column_values``
+    """
+    return int_hash(concat_columns(column_values))
+
+
 ColName = str
 
 
+@dataclass
+class FuncCols:
+    """Simple mapping from function to the columns the function should operate on."""
+
+    func: Callable
+    columns: Tuple[ColName, ...]
+
+
 class CsvDef(TypedDict):
-    dialect: Dialect
+    dialect: Type[Dialect]
     fieldnames: Tuple[ColName, ...]
-    composite: Dict[ColName, Tuple[ColName, ...]]
+    derived: Dict[ColName, FuncCols]
 
 
 CSV_DEFINITIONS: Dict[FileStem, CsvDef] = {
@@ -84,7 +150,10 @@ CSV_DEFINITIONS: Dict[FileStem, CsvDef] = {
             "reference_date",
             "frequency",
         ),
-        "composite": {},
+        # We don't actually need a `cdc_id` derived column for this table. However to ensure we
+        # can call the `PgComparatorCDCOperator` on all tables with the same set of parameters,
+        # they all need a `cdc_id`.
+        "derived": {"cdc_id": FuncCols(hash_columns, ("variabele",))},
     },
     "bbga_latest_and_greatest": {
         "dialect": bbga_dialect,
@@ -94,7 +163,18 @@ CSV_DEFINITIONS: Dict[FileStem, CsvDef] = {
             "indicator_definitie_id",
             "waarde",
         ),
-        "composite": {"id": ("indicator_definitie_id", "jaar", "gebiedcode_15")},
+        # As this table has over 6 million rows, the `cdc_id` derived column becomes a necessity
+        # for us to be able to call `PgComparatorCDCOperator` with the incredibly faster
+        # `--pg-copy` option. That option requires a key column with an integer value. That is
+        # what we define here.
+        #
+        # The `id` column here is by virtue of an `identifier` property in the schema.
+        # Schema-tools automatically creates an `id` column of type VARCHAR for us to fill with
+        # a value derived from the columns specified in the `identifier` property.
+        "derived": {
+            "id": FuncCols(concat_columns, ("indicator_definitie_id", "jaar", "gebiedcode_15")),
+            "cdc_id": FuncCols(hash_columns, ("indicator_definitie_id", "jaar", "gebiedcode_15")),
+        },
     },
     "bbga_std_latest_and_greatest": {
         "dialect": bbga_dialect,
@@ -105,14 +185,23 @@ CSV_DEFINITIONS: Dict[FileStem, CsvDef] = {
             "standaardafwijking",
             "bron",
         ),
-        "composite": {"id": ("indicator_definitie_id", "jaar")},
+        # We don't actually need a `cdc_id` derived column for this table. However to ensure we
+        # can call the `PgComparatorCDCOperator` on all tables with the same set of parameters,
+        # they all need a `cdc_id`.
+        #
+        # The `id` column here is by virtue of an `identifier` property in the schema.
+        # Schema-tools automatically creates an `id` column of type VARCHAR for us to fill with
+        # a value derived from the columns specified in the `identifier` property.
+        "derived": {
+            "id": FuncCols(concat_columns, ("indicator_definitie_id", "jaar")),
+            "cdc_id": FuncCols(hash_columns, ("indicator_definitie_id", "jaar")),
+        },
     },
 }
 
 assert set(table_mappings.keys()) == set(
     CSV_DEFINITIONS.keys()
 ), "Both mappings should have the same set of keys."
-
 
 with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
     slack_at_start = MessageOperator(
@@ -141,9 +230,27 @@ with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
         for file_stem, url_path in data_endpoints.items()
     ]
 
+    def rm_tmp_tables(postfix: str) -> PostgresOperator:
+        return PostgresOperator(
+            task_id=f"rm_tmp_tables{postfix}",
+            sql="DROP TABLE IF EXISTS {tables}".format(
+                tables=", ".join(map(lambda s: f"{TMP_TABLE_PREFIX}{s}", table_mappings.values()))
+            ),
+        )
+
+    rm_tmp_tables_pre = rm_tmp_tables("_pre")
+
     sqlalchemy_create_objects_from_schema = SqlAlchemyCreateObjectOperator(
         task_id="sqlalchemy_create_objects_from_schema", data_schema_name=DAG_ID
     )
+
+    add_cdc_ids = [
+        PostgresOperator(
+            task_id=f"add_cdc_id_to_{table}",
+            sql=f"ALTER TABLE {table} ADD COLUMN IF NOT EXISTS cdc_id BIGINT UNIQUE NOT NULL ",
+        )
+        for table in table_mappings.values()
+    ]
 
     postgres_create_tables_like = PostgresCreateTablesLikeOperator(
         task_id="postgres_create_tables_like",
@@ -151,7 +258,7 @@ with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
         prefix=TMP_TABLE_PREFIX,
     )
 
-    def _transform_csv_files(**kwargs) -> None:
+    def _transform_csv_files(**kwargs: Any) -> None:
         """Transform CSV files to have suitable headers and columns for DB insertion.
 
         The previously downloaded CSV files do not necessarily have column names that can be
@@ -174,9 +281,13 @@ with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
                 reader = csv.DictReader(
                     src, fieldnames=csv_def["fieldnames"], dialect=csv_def["dialect"]
                 )
-                next(reader)  # skip header
+                # Ignore the existing header and replace it with a header of our own that matches
+                # the columns names of table this CSV is going to be inserted in.
+                #
+                # TODO We should probably add a sanity check here before replacing the header
+                next(reader)  # skip existing header
                 output_fieldnames = tuple(
-                    itertools.chain(csv_def["fieldnames"], csv_def["composite"].keys())
+                    itertools.chain(csv_def["fieldnames"], csv_def["derived"].keys())
                 )
 
                 writer = csv.DictWriter(
@@ -184,10 +295,9 @@ with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
                 )
                 writer.writeheader()
                 for row in reader:
-                    for col_name in csv_def["composite"]:
-                        row[col_name] = "|".join(
-                            map(lambda cn: row[cn], csv_def["composite"][col_name])
-                        )
+                    # Calculate derived columns
+                    for col_name, func_cols in csv_def["derived"].items():
+                        row[col_name] = func_cols.func(*map(lambda cn: row[cn], func_cols.columns))
                     writer.writerow(row)
             Path(dst.name).rename(csv_file)
 
@@ -210,18 +320,16 @@ with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
             task_id=f"change_data_capture_{table}",
             source_table=f"{TMP_TABLE_PREFIX}{table}",
             target_table=table,
+            use_pg_copy=True,
+            key_column="cdc_id",
+            use_key=True,
         )
         for table in table_mappings.values()
     ]
 
-    rm_tmp_tables = PostgresOperator(
-        task_id="rm_tmp_tables",
-        sql="DROP TABLE IF EXISTS {tables}".format(
-            tables=", ".join(map(lambda s: f"{TMP_TABLE_PREFIX}{s}", table_mappings.values()))
-        ),
-    )
+    rm_tmp_tables_post = rm_tmp_tables("_post")
 
-    def _rm_tmp_dir(**kwargs) -> None:
+    def _rm_tmp_dir(**kwargs: Any) -> None:
         tmp_dir = kwargs["task_instance"].xcom_pull(task_ids="mk_tmp_dir")
         shutil.rmtree(tmp_dir)
 
@@ -236,11 +344,13 @@ with DAG(dag_id=DAG_ID, default_args=default_args) as dag:
         >> mk_tmp_dir
         >> download_data
         # Ensure we don't have any lingering tmp tables from a previously failed run.
-        >> rm_tmp_tables
+        >> rm_tmp_tables_pre
         >> sqlalchemy_create_objects_from_schema
+        >> add_cdc_ids
         >> postgres_create_tables_like
         >> transform_csv_files
         >> postgres_insert_csv
         >> change_data_capture
+        >> rm_tmp_tables_post
         >> rm_tmp_dir
     )
