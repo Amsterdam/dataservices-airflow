6ceebd0bed59ac39860d9c45c57f18358c15b11c
Guido Kollerie <guido@kollerie.com>
2021-02-16 17:18:52 +0100

    Deprecate `check_safe_name`. [DS-559]
    There is no need for it if one simply uses `psycopg2.sql`.

--- src/plugins/check_helpers.py
+++ src/plugins/check_helpers.py
@@ -1,19 +1,22 @@
 import re
+import warnings
+
 from attrdict import AttrDict
 
 RE_SAFE_IDENTIFIER = re.compile(r"\A[a-z][a-z0-9_\-]+\Z", re.I)
 
 
-def check_safe_name(sql_identifier):
+def check_safe_name(sql_identifier: str) -> None:
     """Check whether an identifier can safely be used inside an SQL statement.
     This avoids causing SQL injections when the identifier ever becomes user-input.
     """
+    warnings.warn("Please use psycopg2.sql instead!", DeprecationWarning)
     if not RE_SAFE_IDENTIFIER.match(sql_identifier):
         raise RuntimeError(f"Unsafe input used as table/field-name: {sql_identifier}")
 
 
 def make_params(checks):
-    """ Collect params from all checks into one dict,
-        because Airflow does the jinja2 interpolation only once per operator run.
+    """Collect params from all checks into one dict,
+    because Airflow does the jinja2 interpolation only once per operator run.
     """
     return AttrDict({check.check_id: check.params for check in checks})
