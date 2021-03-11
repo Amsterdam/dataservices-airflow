1558f6d6a5cd349266589d5a1a8abfd2f3d0a786
Bart Heupers <bart.heupers@gmail.com>
2021-03-11 16:20:13 +0100

    Create geopackage and upload to datacatalogus for beheerkaart
    This commit contain bothe the generic functionality to
    upload a file to a specific resource within a named dataset
    in the datacatalogus, as well as creating a geopackage for the
    beheerkaart, zipping this and upload this to the datacatalogus.

--- src/dags/beheerkaart.py
+++ src/dags/beheerkaart.py
@@ -1,26 +1,40 @@
 from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from bash_env_operator import BashEnvOperator
 from swift_load_sql_operator import SwiftLoadSqlOperator
 from provenance_rename_operator import ProvenanceRenameOperator
 from provenance_drop_from_schema_operator import ProvenanceDropFromSchemaOperator
 from swap_schema_operator import SwapSchemaOperator
+from dcat_swift_operator import DCATSwiftOperator
 
 from common import (
     default_args,
     DATAPUNT_ENVIRONMENT,
+    SHARED_DIR,
     slack_webhook_token,
     MessageOperator,
 )
 
-DATASTORE_TYPE = (
-    "acceptance" if DATAPUNT_ENVIRONMENT == "development" else DATAPUNT_ENVIRONMENT
-)
+from common.db import fetch_pg_env_vars
+
+DATASTORE_TYPE = "acceptance" if DATAPUNT_ENVIRONMENT == "development" else DATAPUNT_ENVIRONMENT
 
 dag_id = "beheerkaart"
+tmp_dir = f"{SHARED_DIR}/{dag_id}"
+tables = "beheerkaart_basis_bgt,beheerkaart_basis_eigendomsrecht,beheerkaart_basis_kaart"
 
+dataset_name = f"{dag_id}_basis"
+gpkg_path = f"{tmp_dir}/{dataset_name}.gpkg"
 
 owner = "team_ruimte"
-with DAG(dag_id, default_args={**default_args, **{"owner": owner}}) as dag:
-
+with DAG(
+    dag_id,
+    default_args={**default_args, **{"owner": owner}},
+    # New data is delivered every wednesday and friday evening,
+    # So we schedule the import on friday and saturday morning
+    schedule_interval="0 0 * * 4,6",
+) as dag:
     # 1. Post message on slack
     slack_at_start = MessageOperator(
         task_id="slack_at_start",
@@ -33,14 +47,16 @@ with DAG(dag_id, default_args={**default_args, **{"owner": owner}}) as dag:
     # 2. Drop tables in target schema PTE (schema which orginates from the DB dump file, see next step)
     #    based upon presence in the Amsterdam schema definition
     drop_tables = ProvenanceDropFromSchemaOperator(
-        task_id="drop_tables", dataset_name="beheerkaart_basis", pg_schema="pte",
+        task_id="drop_tables",
+        dataset_name=dataset_name,
+        pg_schema="pte",
     )
 
     # 3. load the dump file
     swift_load_task = SwiftLoadSqlOperator(
         task_id="swift_load_task",
         container="Dataservices",
-        object_id=f"beheerkaart_pr/beheerkaart_basis/{DATASTORE_TYPE}/bkt.zip",
+        object_id=f"beheerkaart_pr/{dataset_name}/{DATASTORE_TYPE}/bkt.zip",
         swift_conn_id="objectstore_dataservices",
         # optionals
         # db_target_schema will create the schema if not present
@@ -50,17 +66,41 @@ with DAG(dag_id, default_args={**default_args, **{"owner": owner}}) as dag:
     # 4. Make the provenance translations
     provenance_renames = ProvenanceRenameOperator(
         task_id="provenance_renames",
-        dataset_name="beheerkaart_basis",
+        dataset_name=dataset_name,
         pg_schema="pte",
         rename_indexes=True,
     )
 
     # 5. Swap tables to target schema public
-    swap_schema = SwapSchemaOperator(
-        task_id="swap_schema", dataset_name="beheerkaart_basis"
+    swap_schema = SwapSchemaOperator(task_id="swap_schema", dataset_name=dataset_name)
+
+    # 6. Create temporary directory
+    mkdir = BashOperator(task_id="mkdir", bash_command=f"mkdir -p {tmp_dir}")
+
+    # 7. Create geopackage
+    create_geopackage = BashEnvOperator(
+        task_id="create_geopackage",
+        env={},
+        env_expander=fetch_pg_env_vars,
+        bash_command=f'ogr2ogr -f GPKG {gpkg_path} PG:"tables={tables}"',
     )
 
+    # 8. Zip geopackage
+    zip_geopackage = BashEnvOperator(
+        task_id="zip_geopackage",
+        env={},
+        env_expander=fetch_pg_env_vars,
+        bash_command=f"zip -j {gpkg_path}.zip {gpkg_path}",
+    )
+
+    # 9. Upload geopackage to datacatalog
+    upload_data = DCATSwiftOperator(
+        environment=DATAPUNT_ENVIRONMENT,
+        task_id=f"upload_data",
+        input_path=f"{gpkg_path}.zip",
+        dataset_title="Beheerkaart basis",
+        distribution_id="1",
+    )
 
 # FLOW
-
-slack_at_start >> drop_tables >> swift_load_task >> provenance_renames >> swap_schema
+slack_at_start >> drop_tables >> swift_load_task >> provenance_renames >> swap_schema >> mkdir >> create_geopackage >> zip_geopackage >> upload_data
--- src/plugins/swift_hook.py
+++ src/plugins/swift_hook.py
@@ -2,15 +2,15 @@ from contextlib import contextmanager
 from pathlib import Path
 from airflow.hooks.base_hook import BaseHook
 from airflow.exceptions import AirflowException
-from swiftclient.service import SwiftService, SwiftError
+from swiftclient.service import SwiftService, SwiftError, SwiftUploadObject
 
 
 class SwiftHook(BaseHook):
-    """ A Swift hook to interact with the objectstore.
-        If no swift_conn_id is provided, the default
-        connection is used. The default connection
-        is defined in OS_USERNAME, OS_PASSWORD, OS_TENANT_NAME
-        and OS_AUTH_URL, as requested by the SwiftService
+    """A Swift hook to interact with the objectstore.
+    If no swift_conn_id is provided, the default
+    connection is used. The default connection
+    is defined in OS_USERNAME, OS_PASSWORD, OS_TENANT_NAME
+    and OS_AUTH_URL, as requested by the SwiftService
     """
 
     def __init__(self, swift_conn_id="swift_default"):
@@ -47,16 +47,46 @@ class SwiftHook(BaseHook):
         with self.connection() as swift:
             try:
                 for down_res in swift.download(
-                    container=container, objects=[object_id], options=download_options,
+                    container=container,
+                    objects=[object_id],
+                    options=download_options,
                 ):
                     if down_res["success"]:
                         self.log.info("downloaded: %s", down_res["object"])
                     else:
                         self.log.error("download failed: %s", down_res["object"])
-                        raise AirflowException(
-                            f"Failed to fetch file: {down_res['object']}"
-                        )
+                        raise AirflowException(f"Failed to fetch file: {down_res['object']}")
 
             except SwiftError as e:
                 self.log.error(e.value)
                 raise AirflowException(f"Failed to fetch file: {e.value}")
+
+    def upload(self, container, local_file, object_id):
+        with self.connection() as swift:
+            try:
+                for r in swift.upload(
+                    container, [SwiftUploadObject(local_file, object_name=object_id)]
+                ):
+                    if r["success"]:
+                        if "object" in r:
+                            self.log.info(f"uploaded: {r['object']}")
+                        elif "for_object" in r:
+                            self.log.info(f"{r['for_object']} segment {r['segment_index']}")
+                    else:
+                        error = r["error"]
+                        if r["action"] == "create_container":
+                            self.log.warning(
+                                "Warning: failed to create container " "'%s'%s", container, error
+                            )
+                        elif r["action"] == "upload_object":
+                            self.log.error(
+                                "Failed to upload object %s to container %s: %s"
+                                % (container, r["object"], error)
+                            )
+                        else:
+                            self.log.error("%s" % error)
+                        raise AirflowException(f"Failed to upload file: {r['object']} ({error})")
+
+            except SwiftError as e:
+                self.log.error(e.value)
+                raise AirflowException(f"Failed to upload file: {e.value}")
