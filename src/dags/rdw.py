19edc95f54945d5d47e1e63f07ed999751aa5465
Chris van Riel <c.van.riel@amsterdam.nl>
2021-04-10 14:37:45 +0200

    Pipeline RDW
    The RDW source contains of - in this particular case - of 3 resources. Each of those resources contains about 15 million records. To make the data processing a fast as possible the data is downloaded as a comma seperated file (csv). This is by far the most efficient approach. Alternatives where several API calls returning the data in batches and commiting them as a batch to the database (consuming a lot of memory). However this took way too much time.

---
+++ src/dags/rdw.py
@@ -0,0 +1,160 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+from common.db import DatabaseEngine
+from environs import Env
+from more_ds.network.url import URL
+from ogr2ogr_operator import Ogr2OgrOperator
+from provenance_rename_operator import ProvenanceRenameOperator
+from postgres_permissions_operator import PostgresPermissionsOperator
+from typing import Dict, Iterable
+
+from common import (
+    default_args,
+    SHARED_DIR,
+    slack_webhook_token,
+    DATAPUNT_ENVIRONMENT,
+    MessageOperator,
+    quote_string,
+)
+
+from importscripts.import_rdw import download_data
+from sql.rdw import SQL_CREATE_TMP_TABLE, SQL_SWAP_TABLE
+
+# Source defaults to 1000 records per request.
+# There is no unlimited.
+DATA_LIMIT: int = 100_000_000
+DATA_SELECTIONS: Dict[str, Iterable[str]] = {
+    "basis": [
+        "kenteken",
+        "voertuigsoort",
+        "massa_rijklaar",
+        "toegestane_maximum_massa_voertuig",
+        "inrichting",
+        "datum_eerste_toelating",
+        "lengte",
+        "maximum_massa_samenstelling",
+    ],
+    "brandstof": ["kenteken", "brandstof_omschrijving", "emissiecode_omschrijving"],
+    "assen": ["kenteken", "aantal_assen", "as_nummer", "technisch_toegestane_maximum_aslast"],
+}
+
+dag_id: str = "rdw"
+description: str = (
+    "Contextuele informatie over een RDW geregistreerd voertuig op basis van het kenteken."
+)
+variables: Dict = Variable.get(dag_id, deserialize_json=True)
+endpoints: Dict = variables["data_endpoints"]
+tmp_dir: str = f"{SHARED_DIR}/{dag_id}"
+db_conn: object = DatabaseEngine()
+env: Env = Env()
+rdw_base_url: str = URL(env("AIRFLOW_CONN_RDW_BASE_URL"))
+
+
+with DAG(
+    dag_id,
+    description=description,
+    default_args=default_args,
+    user_defined_filters=dict(quote=quote_string),
+) as dag:
+
+    # 1. Post message on slack
+    slack_at_start = MessageOperator(
+        task_id="slack_at_start",
+        http_conn_id="slack",
+        webhook_token=slack_webhook_token,
+        message=f"Starting {dag_id} ({DATAPUNT_ENVIRONMENT})",
+        username="admin",
+    )
+
+    # 2. Create temp directory to store files
+    mkdir = BashOperator(task_id="mkdir", bash_command=f"mkdir -p {tmp_dir}")
+
+    # 3. Download csv
+    download_data = [
+        PythonOperator(
+            task_id=f"download_{resource}",
+            python_callable=download_data,
+            op_kwargs=dict(
+                endpoint=f"{URL(rdw_base_url) / endpoint // dict({'$select': {','.join(DATA_SELECTIONS[resource])}}) // dict({'$LIMIT': {DATA_LIMIT}})}",  # noqa E501
+                output_file=f"{tmp_dir}/{resource}.csv",
+            ),
+        )
+        for resource, endpoint in endpoints.items()
+    ]
+
+    # 4. Import csv into database
+    import_data = [
+        Ogr2OgrOperator(
+            task_id=f"import_{resource}",
+            target_table_name=f"{dag_id}_{resource}_new",
+            input_file=f"{tmp_dir}/{resource}.csv",
+            s_srs=None,
+            auto_detect_type="YES",
+            mode="PostgreSQL",
+            fid="id",
+            db_conn=db_conn,
+        )
+        for resource in endpoints
+    ]
+
+    # 5. SETUP tmp TABLE
+    create_tmp_table = PostgresOperator(
+        task_id="create_tmp_table",
+        sql=SQL_CREATE_TMP_TABLE,
+        params=dict(tablename=f"{dag_id}_{dag_id}_new"),
+    )
+
+    # 6. Rename COLUMNS based on provenance (if specified)
+    provenance = ProvenanceRenameOperator(
+        task_id="provenance_col_rename",
+        dataset_name=dag_id,
+        prefix_table_name=f"{dag_id}_",
+        postfix_table_name="_new",
+        rename_indexes=False,
+        pg_schema="public",
+    )
+
+    # 7. SWAP tmp to target (no CDC because of size)
+    swap_table = PostgresOperator(
+        task_id="swap_table",
+        sql=SQL_SWAP_TABLE,
+        params=dict(tablename=f"{dag_id}_{dag_id}"),
+    )
+
+    # 8. Remove downloaded files
+    clean_up_files = BashOperator(task_id="clean_up_files", bash_command=f"rm -r {tmp_dir}/*")
+
+    # 9. Grant database permissions
+    grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=dag_id)
+
+# FLOW
+
+slack_at_start >> mkdir >> download_data
+
+for (get_data, load_data) in zip(download_data, import_data):
+
+    [
+        get_data >> load_data
+    ] >> create_tmp_table >> provenance >> swap_table >> clean_up_files >> grant_db_permissions
+
+
+dag.doc_md = """
+    #### DAG summary
+    This DAG contains data about RDW registered vehicles.
+    #### Mission Critical
+    Classified as 2 (beschikbaarheid [range: 1,2,3])
+    #### On Failure Actions
+    Fix issues and rerun dag on working days
+    #### Point of Contact
+    Inform the businessowner at [businessowner]@amsterdam.nl
+    #### Business Use Case / process / origin
+    Na
+    #### Prerequisites/Dependencies/Resourcing
+    https://api.data.amsterdam.nl/v1/docs/datasets/rdw.html
+    https://api.data.amsterdam.nl/v1/docs/wfs-datasets/rdw.html
+    Example geosearch:
+    N.A.
+"""
