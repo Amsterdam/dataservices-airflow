3c5802b48beb9cd1adb8117b13f9b465873954a0
jjmurre <jan.murre@gmail.com>
2021-06-01 16:39:39 +0200

    Check for broken dags during deployment

---
+++ src/scripts/checkdags.py
@@ -0,0 +1,21 @@
+"""Python script to check for broken DAGs.
+
+This scripts is running during the initialization and startup of Airflow during deployment.
+When DAGs are broken, this results in Airflow not being able to start.
+So, broken dags are detected during deployment.
+"""
+
+from pathlib import Path
+import subprocess
+
+
+dagsfolder = Path(__file__).parent / ".." / "dags"
+
+broken_dags = []
+for dagfile in dagsfolder.glob("*.py"):
+    proc = subprocess.run(["python", dagfile])
+    if proc.returncode != 0:
+        broken_dags.append(dagfile.name)
+
+if broken_dags:
+    raise Exception("These dags are broken: {}".format(", ".join(broken_dags)))
