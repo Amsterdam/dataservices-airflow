40d1f526d431a12fb0fe0e87ffff923bf4c05f6f
Guido Kollerie <guido@kollerie.com>
2021-06-25 14:10:38 +0200

    AB#8704 Use upper case names for constants.
    Per PEP 008. While we are at it annotate them with `Final` so that MyPy
    can verify that are actually used as constants.

--- src/dags/cmsa.py
+++ src/dags/cmsa.py
@@ -21,13 +21,11 @@ from postgres_permissions_operator import PostgresPermissionsOperator
 from provenance_rename_operator import ProvenanceRenameOperator
 from swift_operator import SwiftOperator
 
-dag_id = "cmsa"
-variables = Variable.get(dag_id, deserialize_json=True)
-files_to_download = variables["files_to_download"]
-tmp_dir = Path(SHARED_DIR) / dag_id
-sql_path = Path(__file__).resolve().parents[0] / "sql"
-fetch_jsons = []
-
+DAG_ID: Final = "cmsa"
+VARIABLES: Final = Variable.get(DAG_ID, deserialize_json=True)
+FILES_TO_DOWNLOAD: Final = VARIABLES["files_to_download"]
+TMP_DIR: Final = Path(SHARED_DIR) / DAG_ID
+SQL_PATH: Final = Path(__file__).resolve().parents[0] / "sql"
 
 SQL_TABLE_RENAMES: Final = """
     ALTER TABLE IF EXISTS cmsa_sensor RENAME TO cmsa_sensor_old;
@@ -45,32 +43,31 @@ SQL_TABLE_RENAMES: Final = """
 
 
 with DAG(
-    dag_id,
+    DAG_ID,
     description="""Crowd Monitoring Systeem Amsterdam:
                 3D sensoren, wifi sensoren, (tel)camera's en beacons""",
     default_args=default_args,
     template_searchpath=["/"],
-    on_failure_callback=get_contact_point_on_failure_callback(dataset_id=dag_id),
+    on_failure_callback=get_contact_point_on_failure_callback(dataset_id=DAG_ID),
 ) as dag:
-
     # 1. Post info message on slack
     slack_at_start = MessageOperator(
         task_id="slack_at_start",
         http_conn_id="slack",
         webhook_token=slack_webhook_token,
-        message=f"Starting {dag_id} ({DATAPUNT_ENVIRONMENT})",
+        message=f"Starting {DAG_ID} ({DATAPUNT_ENVIRONMENT})",
         username="admin",
     )
 
     # 2. Create temp directory to store files
-    mkdir = mk_dir(tmp_dir)
+    mkdir = mk_dir(TMP_DIR)
 
     # 3. Download sensor data (geojson) from maps.amsterdam.nl
     download_geojson = HttpFetchOperator(
         task_id="download_geojson",
         endpoint="open_geodata/geojson.php?KAARTLAAG=CROWDSENSOREN&THEMA=cmsa",
         http_conn_id="ams_maps_conn_id",
-        tmp_file=tmp_dir / "sensors.geojson",
+        tmp_file=TMP_DIR / "sensors.geojson",
     )
 
     # 4. Download additional data (beacons.csv, cameras.xlsx)
@@ -81,9 +78,9 @@ with DAG(
             # swift_conn_id="SWIFT_DEFAULT",
             container="cmsa",
             object_id=file,
-            output_path=tmp_dir / file,
+            output_path=TMP_DIR / file,
         )
-        for file in files_to_download
+        for file in FILES_TO_DOWNLOAD
     ]
 
     # 5. Create SQL insert statements out of downloaded data
@@ -91,39 +88,39 @@ with DAG(
         task_id="proces_sensor_data",
         python_callable=import_cmsa,
         op_args=[
-            tmp_dir / "cameras.xlsx",
-            tmp_dir / "beacons.csv",
-            tmp_dir / "sensors.geojson",
-            tmp_dir,
+            TMP_DIR / "cameras.xlsx",
+            TMP_DIR / "beacons.csv",
+            TMP_DIR / "sensors.geojson",
+            TMP_DIR,
         ],
     )
 
     # 6. Create target tables: Sensor en Locatie
     create_tables = PostgresFilesOperator(
         task_id="create_target_tables",
-        sql_files=[f"{sql_path}/cmsa_data_create.sql"],
+        sql_files=[f"{SQL_PATH}/cmsa_data_create.sql"],
     )
 
     # 7. Insert data into DB
     import_data = PostgresFilesOperator(
         task_id="import_data_into_DB",
         sql_files=[
-            tmp_dir / "cmsa_sensor_new.sql",
-            tmp_dir / "cmsa_locatie_new.sql",
+            TMP_DIR / "cmsa_sensor_new.sql",
+            TMP_DIR / "cmsa_locatie_new.sql",
         ],
     )
 
     # 8. Create target tables: Markering (join between Sensor en Locatie)
     fill_markering = PostgresFilesOperator(
         task_id="insert_into_table_markering",
-        sql_files=[f"{sql_path}/cmsa_data_insert_markering.sql"],
+        sql_files=[f"{SQL_PATH}/cmsa_data_insert_markering.sql"],
     )
 
     # 9. RENAME columns based on PROVENANCE
     provenance_translation = ProvenanceRenameOperator(
         task_id="provenance_rename",
-        dataset_name=dag_id,
-        prefix_table_name=f"{dag_id}_",
+        dataset_name=DAG_ID,
+        prefix_table_name=f"{DAG_ID}_",
         postfix_table_name="_new",
         rename_indexes=False,
         pg_schema="public",
@@ -133,23 +130,21 @@ with DAG(
     rename_tables = PostgresOperator(task_id="rename_tables", sql=SQL_TABLE_RENAMES)
 
     # 11. Grant database permissions
-    grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=dag_id)
+    grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=DAG_ID)
 
-
-(
-    slack_at_start
-    >> mkdir
-    >> download_geojson
-    >> fetch_files
-    >> proces_cmsa
-    >> create_tables
-    >> import_data
-    >> fill_markering
-    >> provenance_translation
-    >> rename_tables
-    >> grant_db_permissions
-)
-
+    (
+        slack_at_start
+        >> mkdir
+        >> download_geojson
+        >> fetch_files
+        >> proces_cmsa
+        >> create_tables
+        >> import_data
+        >> fill_markering
+        >> provenance_translation
+        >> rename_tables
+        >> grant_db_permissions
+    )
 
 dag.doc_md = """
     #### DAG summary
