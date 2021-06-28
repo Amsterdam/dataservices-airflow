640c0794f7181fb51db830c73819be8b5209b50a
Chris van Riel <c.van.riel@amsterdam.nl>
2021-06-28 13:06:16 +0200

    OGR2OGR promote_to_multi fix
    The promote_to_multi option was not optional yet. Turn it into an boolean value where de default is set to False (not promoting geometry objects to multigeometry objects).
    
    Currently it was leading to errors in Airflow because it was defaulted to always promote. This lead to geometry check issues.

--- src/dags/bouwstroompunten.py
+++ src/dags/bouwstroompunten.py
@@ -121,6 +121,7 @@ with DAG(
         s_srs=None,
         fid="ogc_fid",
         auto_detect_type="YES",
+        promote_to_multi=False,
         mode="PostgreSQL",
         db_conn=db_conn,
     )
