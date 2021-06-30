afc2fab1d8d0a8667af96035071df7c15df1676f
Chris van Riel <c.van.riel@amsterdam.nl>
2021-06-30 17:55:56 +0200

    stroomstoringen additions
    - Weesp is added to the source query request.
    - Source data is imported by ogr2ogr. If the source result set is empty (what can happen if there are no electic blackouts), it will create an target table missing some key columns. If so, the table is created by using the schema definition instead and a dummy record is inserted indication no data (on request).
    
    The DAG uses the trigger rules concept provided by Airflow (trough inheritance of the baseoperator). The steps can therefore be triggered according to the set rule e.g. all_success or one_success etc.

--- src/dags/stroomstoringen.py
+++ src/dags/stroomstoringen.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Final
+from typing import Dict, Final
 
 import pendulum
 from airflow import DAG
@@ -22,28 +22,33 @@ from ogr2ogr_operator import Ogr2OgrOperator
 from postgres_permissions_operator import PostgresPermissionsOperator
 from postgres_rename_operator import PostgresTableRenameOperator
 from provenance_rename_operator import ProvenanceRenameOperator
-from sql.stroomstoringen import CONVERT_DATE_TIME
+from sql.stroomstoringen import (
+    CHECK_TABLE,
+    CONVERT_DATE_TIME,
+    DROP_TABLE_IF_EXISTS,
+    NO_DATA_PRESENT_INDICATOR,
+)
+from sqlalchemy_create_object_operator import SqlAlchemyCreateObjectOperator
 
-dag_id: str = "stroomstoringen"
-variables: dict = Variable.get("stroomstoringen", deserialize_json=True)
+DAG_ID: Final = "stroomstoringen"
+variables: Dict[str, Dict[str, str]] = Variable.get("stroomstoringen", deserialize_json=True)
 endpoint_url: str = variables["data_endpoints"]["stroomstoringen"]
 
-
 TODAY: Final = pendulum.now(TIMEZONE).format("MM-DD-YYYY")
-TMP_DIR: str = f"{SHARED_DIR}/{dag_id}"
+TMP_DIR: Final = Path(SHARED_DIR) / DAG_ID
 
 db_conn = DatabaseEngine()
 
 
 with DAG(
-    dag_id,
+    DAG_ID,
     description="locaties / gebieden stroomstoringen electra netwerk Liander.",
     default_args=default_args,
     user_defined_filters={"quote": quote_string},
     template_searchpath=["/"],
     schedule_interval="*/10 * * * *",
     catchup=False,
-    on_failure_callback=get_contact_point_on_failure_callback(dataset_id=dag_id),
+    on_failure_callback=get_contact_point_on_failure_callback(dataset_id=DAG_ID),
 ) as dag:
 
     # 1. Post info message on slack
@@ -51,12 +56,12 @@ with DAG(
         task_id="slack_at_start",
         http_conn_id="slack",
         webhook_token=slack_webhook_token,
-        message=f"Starting {dag_id} ({DATAPUNT_ENVIRONMENT})",
+        message=f"Starting {DAG_ID} ({DATAPUNT_ENVIRONMENT})",
         username="admin",
     )
 
     # 2. Create temp directory to store files
-    mkdir = mk_dir(Path(TMP_DIR))
+    mkdir = mk_dir(TMP_DIR, clean_if_exists=False)
 
     # 3. download the data into temp directorys
     download_data = HttpFetchOperator(
@@ -67,10 +72,10 @@ with DAG(
         output_type="text",
     )
 
-    # 4. Create SQL
+    # 4. Import data
     import_data = Ogr2OgrOperator(
         task_id="import_data",
-        target_table_name=f"{dag_id}_{dag_id}_new",
+        target_table_name=f"{DAG_ID}_{DAG_ID}_new",
         input_file=f"{TMP_DIR}/stroomstoringen.geojson",
         auto_detect_type="YES",
         mode="PostgreSQL",
@@ -80,39 +85,90 @@ with DAG(
         db_conn=db_conn,
     )
 
-    # 5. convert epoch time
+    # 5. Check target table if source data structure is loaded
+    # NOTE: This task has zero retries. It is used to determine
+    # what path to executed based on it's outcome.
+    check_table = PostgresOperator(
+        task_id="check_table",
+        sql=CHECK_TABLE,
+        params={"tablename": f"{DAG_ID}_{DAG_ID}_new"},
+        retries=0,
+    )
+
+    # 6. Drop table if exists
+    # NOTE: This triggers only if upstream task has failed.
+    drop_table = PostgresOperator(
+        task_id="drop_table",
+        sql=DROP_TABLE_IF_EXISTS,
+        params={"tablename": f"{DAG_ID}_{DAG_ID}_new"},
+        trigger_rule="all_failed",
+    )
+
+    # 7. Create the DB target temp table (based on the JSON data schema)
+    # if table not exists yet
+    # this ensures that when the source does not contain any data at a particular
+    # moment (there can be no electric blackout at the current time) the table is still
+    # created and the indication no data is present can be added.
+    create_table = SqlAlchemyCreateObjectOperator(
+        task_id="create_table_based_upon_schema",
+        data_schema_name=DAG_ID,
+        data_table_name=f"{DAG_ID}_{DAG_ID}",
+        db_table_name=f"{DAG_ID}_{DAG_ID}_new",
+        ind_table=True,
+        # when set to false, it doesn't create indexes; only tables
+        ind_extra_index=True,
+    )
+
+    # 8. If source has no data add dummy record (indication no data present)
+    no_data_indicator = PostgresOperator(
+        task_id="no_data_indicator",
+        sql=NO_DATA_PRESENT_INDICATOR,
+        params={"tablename": f"{DAG_ID}_{DAG_ID}_new"},
+    )
+
+    # 9. convert epoch time
+    # NOTE: This triggers only if upstream task has been succesful.
     convert_datetime = PostgresOperator(
         task_id="convert_datetime",
         sql=CONVERT_DATE_TIME,
-        params={"tablename": f"{dag_id}_{dag_id}_new"},
+        params={"tablename": f"{DAG_ID}_{DAG_ID}_new"},
+        trigger_rule="all_success",
     )
 
-    # 6. Rename COLUMNS based on Provenance
+    # 10. Rename COLUMNS based on Provenance
+    # NOTE: This triggers if one of the parent upstream tasks has been succesful.
     provenance_translation = ProvenanceRenameOperator(
         task_id="rename_columns",
-        dataset_name=dag_id,
-        prefix_table_name=f"{dag_id}_",
+        dataset_name=DAG_ID,
+        prefix_table_name=f"{DAG_ID}_",
         postfix_table_name="_new",
         rename_indexes=False,
         pg_schema="public",
+        trigger_rule="one_success",
     )
 
-    # 7. Rename TABLE
+    # 11. Rename TABLE
     rename_table = PostgresTableRenameOperator(
         task_id="rename_table",
-        old_table_name=f"{dag_id}_{dag_id}_new",
-        new_table_name=f"{dag_id}_{dag_id}",
+        old_table_name=f"{DAG_ID}_{DAG_ID}_new",
+        new_table_name=f"{DAG_ID}_{DAG_ID}",
     )
 
-    # 8. Grant database permissions
-    grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=dag_id)
+    # 12. Grant database permissions
+    grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=DAG_ID)
+
+
+# FLOW
+slack_at_start >> mkdir >> download_data >> import_data >> check_table
+
+# Based on outcome check_table
+# path 1: Case: Source has no data, so table is created and dummy record is inserted
+[check_table >> drop_table >> create_table >> no_data_indicator]
+# path 2: Case: Source has data, so processed accordingly
+[check_table >> convert_datetime]
 
 (
-    slack_at_start
-    >> mkdir
-    >> download_data
-    >> import_data
-    >> convert_datetime
+    [no_data_indicator, convert_datetime]
     >> provenance_translation
     >> rename_table
     >> grant_db_permissions
