a64d389cba5e5f087d2740e1eaea09e44d818f80
Guido Kollerie <guido@kollerie.com>
2021-07-11 14:34:52 +0200

    AB#8702 Rewrite basiskaart to be one DAG with parallel tasks.
    In preparation for schema based table creation I have changed the
    dynamic DAG creation (a DAG per table) to be a regular single statically
    defined DAG where all table operations are now parallel tasks. Reason
    being that the `SqlAlchemyCreateObjectOperator` creates all tables in
    the schema at once. Hence only needs to be called once and not once in
    every dynamically created DAG.
    
    NOTE: the `SqlAlchemyCreateObjectOperator` is not yet used in this
    commit. This commit is just pre-work.

--- src/dags/basiskaart.py
+++ src/dags/basiskaart.py
@@ -3,12 +3,12 @@ from typing import Final, Iterator
 
 from airflow import DAG
 from airflow.models import Variable
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from common import DATAPUNT_ENVIRONMENT, MessageOperator, default_args, env, slack_webhook_token
 from contact_point.callbacks import get_contact_point_on_failure_callback
-from dynamic_dagrun_operator import TriggerDynamicDagRunOperator
 from pgcomparator_cdc_operator import PgComparatorCDCOperator
 from postgres_permissions_operator import PostgresPermissionsOperator
 from provenance_rename_operator import ProvenanceRenameOperator
@@ -37,11 +37,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
 DAG_ID: Final = "basiskaart"
-OWNER: Final = "dataservices"
 VARIABLES: Final = Variable.get(DAG_ID, deserialize_json=True)
 TABLES_TO_CREATE: Final = VARIABLES["tables_to_create"]
 SOURCE_CONNECTION: Final = "AIRFLOW_CONN_POSTGRES_BASISKAART"
 IMPORT_STEP: Final = 10_000
+
 logger = logging.getLogger(__name__)
 
 
@@ -82,7 +82,7 @@ def copy_data_in_batch(target_base_table: str, fetch_iterator: Iterator) -> int:
 
     # setup SQL insert values the be executed
     for row in fetch_iterator:
-        items.append([column_value for column_value in row])
+        items.append(list(row))
 
     result = len(items)
 
@@ -100,53 +100,45 @@ def copy_data_in_batch(target_base_table: str, fetch_iterator: Iterator) -> int:
     return result
 
 
-def create_basiskaart_dag(is_first: bool, table_name: str, select_statement: str) -> DAG:
-    """Generates a DAG for each table.
+def join(number: int) -> DummyOperator:
+    """Return numbered DummyOperator to join parallel tasks."""
+    return DummyOperator(task_id=f"join_parallel_tasks_{number}")
 
-    The table_name is the target table in de masterDB where the data will be inserted. The
-    select_statement is one of the imported SQL query selects (see above) that will be executed
-    on the source DB.
-    """
-    # start time first DAG
-    # Note: the basiskaartimport task in Jenkins runs at an arbitrary but invariant time between
-    # 3 and 5 a.m. Because of this, the first DAG starts running at 7 a.m.
-    schedule_start_hour = 7
 
-    dag = DAG(
-        f"{DAG_ID}_{table_name}",
-        default_args={"owner": OWNER, **default_args},
-        # the first DAG will have the is_first boolean set to True
-        # the other DAG's will be triggered to start when the previous DAG is finished
-        # (estafette run / relay run)
-        schedule_interval=f"0 {schedule_start_hour} * * *" if is_first else None,
-        description="""
-        basisregistratie grootschalige topologie (BGT) en kleinschalige basiskaart (KBK10 en 50).
+with DAG(
+    dag_id=DAG_ID,
+    default_args=default_args,
+    # Note: the basiskaartimport task in Jenkins runs at an arbitrary but invariant time
+    # between 3 and 5 a.m. Because of this, the first DAG starts running at 7 a.m.
+    schedule_interval="0 7 * * *",
+    description="""
+        Basisregistratie grootschalige topologie (BGT) en kleinschalige basiskaart (KBK10 en 50).
         The basiskaart data is collected from basiskaart DB.""",
-        tags=["basiskaart"],
-        on_failure_callback=get_contact_point_on_failure_callback(dataset_id=DAG_ID),
+    tags=["basiskaart"],
+    on_failure_callback=get_contact_point_on_failure_callback(dataset_id=DAG_ID),
+) as dag:
+    slack_at_start = MessageOperator(
+        task_id="slack_at_start",
+        http_conn_id="slack",
+        webhook_token=slack_webhook_token,
+        message=f"Starting {DAG_ID} ({DATAPUNT_ENVIRONMENT})",
+        username="admin",
     )
 
-    with dag:
-
-        # 1. Post info message on slack
-        slack_at_start = MessageOperator(
-            task_id="slack_at_start",
-            http_conn_id="slack",
-            webhook_token=slack_webhook_token,
-            message=f"Starting {DAG_ID} ({DATAPUNT_ENVIRONMENT})",
-            username="admin",
-        )
-
-        # 2. Create temp and target table
-        create_tables = PostgresOperator(
-            task_id="create_tables",
+    create_tables = [
+        PostgresOperator(
+            task_id=f"create_table_{table_name}",
             sql=CREATE_TABLES,
             params={"base_table": table_name, "dag_id": DAG_ID},
         )
+        for table_name in TABLES_TO_CREATE.keys()
+    ]
 
-        # 3. Copy data into temp table
-        copy_data = PythonOperator(
-            task_id="insert_data",
+    join_1 = join(1)
+
+    copy_data = [
+        PythonOperator(
+            task_id=f"insert_data_into_{table_name}",
             python_callable=create_tables_from_basiskaartdb_to_masterdb,
             op_kwargs={
                 "source_connection": SOURCE_CONNECTION,
@@ -155,84 +147,84 @@ def create_basiskaart_dag(is_first: bool, table_name: str, select_statement: str
             },
             dag=dag,
         )
+        for table_name, select_statement in TABLES_TO_CREATE.items()
+    ]
 
-        # 4. Check for changes in temp table to merge in target table
-        change_data_capture = PgComparatorCDCOperator(
-            task_id="change_data_capture",
+    join_2 = join(2)
+
+    change_data_capture = [
+        PgComparatorCDCOperator(
+            task_id=f"change_data_capture_for_{table_name}",
             source_table=f"{DAG_ID}_{table_name}_temp",
             target_table=f"{DAG_ID}_{table_name}",
         )
+        for table_name in TABLES_TO_CREATE.keys()
+    ]
 
-        # 5. Create mviews for T-REX tile server
-        create_mviews = PostgresOperator(
-            task_id="create_mviews",
+    join_3 = join(3)
+
+    # Create mviews for T-REX tile server
+    create_mviews = [
+        PostgresOperator(
+            task_id=f"create_mviews_for_{table_name}",
             sql=CREATE_MVIEWS,
             params={"base_table": table_name, "dag_id": DAG_ID},
         )
+        for table_name in TABLES_TO_CREATE.keys()
+    ]
 
-        # 6. Rename COLUMNS based on Provenance
-        provenance_translation = ProvenanceRenameOperator(
-            task_id="rename_columns",
-            dataset_name=DAG_ID,
-            prefix_table_name=f"{DAG_ID}_",
-            rename_indexes=False,
-            pg_schema="public",
-        )
+    provenance_translation = ProvenanceRenameOperator(
+        task_id="rename_columns",
+        dataset_name=DAG_ID,
+        prefix_table_name=f"{DAG_ID}_",
+        rename_indexes=False,
+        pg_schema="public",
+    )
 
-        # 7. Drop temp table
-        clean_up = PostgresOperator(
-            task_id="drop_temp_table",
+    clean_up = [
+        PostgresOperator(
+            task_id=f"drop_temp_table_{table_name}",
             sql=[
                 f"DROP TABLE IF EXISTS {DAG_ID}_{table_name}_temp CASCADE",
             ],
         )
+        for table_name in TABLES_TO_CREATE.keys()
+    ]
 
-        # 8. Trigger next DAG to run (estafette)
-        trigger_next_dag = TriggerDynamicDagRunOperator(
-            task_id="trigger_next_dag",
-            dag_id_prefix=f"{DAG_ID}_",
-            trigger_rule="all_done",
-        )
-
-        # 9. Grant database permissions
-        grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=DAG_ID)
+    grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=DAG_ID)
 
     # Flow
     (
         slack_at_start
         >> create_tables
+        >> join_1
         >> copy_data
+        >> join_2
         >> change_data_capture
+        >> join_3
         >> create_mviews
         >> provenance_translation
         >> clean_up
-        >> trigger_next_dag
         >> grant_db_permissions
     )
 
     dag.doc_md = """
-    #### DAG summary
-    This DAG contains BGT (basisregistratie grootschalige topografie) i
-    and KBK10 (kleinschalige basiskaart 10)
-    and KBK50 (kleinschalige basiskaart 50) data
-    #### Mission Critical
-    Classified as 2 (beschikbaarheid [range: 1,2,3])
-    #### On Failure Actions
-    Fix issues
-    and rerun dag on working days
-    #### Point of Contact
-    Inform the businessowner at [businessowner]@amsterdam.nl
-    #### Business Use Case / process / origin
-    NA
-    #### Prerequisites/Dependencies/Resourcing
-    https://api.data.amsterdam.nl/v1/docs/datasets/basiskaart.html
-    Note: The basiskaart data is collected from the GOB objectstore
-    and processed in the basiskaart DB
-    => which is the source for this DAG.
-    """
-
-    return dag
-
-
-for i, (table, select_statement) in enumerate(TABLES_TO_CREATE.items()):
-    globals()[f"{DAG_ID}_{table}"] = create_basiskaart_dag(i == 0, table, select_statement)
+        #### DAG summary
+        This DAG contains BGT (basisregistratie grootschalige topografie) i
+        and KBK10 (kleinschalige basiskaart 10)
+        and KBK50 (kleinschalige basiskaart 50) data
+        #### Mission Critical
+        Classified as 2 (beschikbaarheid [range: 1,2,3])
+        #### On Failure Actions
+        Fix issues
+        and rerun dag on working days
+        #### Point of Contact
+        Inform the businessowner at [businessowner]@amsterdam.nl
+        #### Business Use Case / process / origin
+        NA
+        #### Prerequisites/Dependencies/Resourcing
+        https://api.data.amsterdam.nl/v1/docs/datasets/basiskaart.html
+        Note: The basiskaart data is collected from the GOB objectstore
+        and processed in the basiskaart DB
+        => which is the source for this DAG.
+        """
