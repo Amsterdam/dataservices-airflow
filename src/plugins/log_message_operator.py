1de2b68150974e79f84a9e6eabb624882f2e56df
Guido Kollerie <guido@kollerie.com>
2021-07-11 14:23:59 +0200

    AB#8702 Clean up LogMessageOperator.
    The `__init__` method had many unused parameters. Turns out they were
    only there to keep them from ending up in `kwargs`. This is error prone
    as it means this  `__init__` needs to be kept in sync with that of the
    `SlackWebhookOperator`. Better to query `BaseOperator`'s `__init__`
    method to see what it accepts and only pass that along.

--- src/plugins/log_message_operator.py
+++ src/plugins/log_message_operator.py
@@ -1,33 +1,34 @@
+import inspect
+from typing import Any, Optional
+
 from airflow.models import BaseOperator
+from airflow.models.taskinstance import Context
 from airflow.utils.decorators import apply_defaults
 
 
 class LogMessageOperator(BaseOperator):
-    """
-    Operator that logs messages. To proxy SlackWebhookOperator for
-    local development.
+    """Log messages to default logger.
+
+    To proxy SlackWebhookOperator for local development.
     """
 
-    @apply_defaults
-    def __init__(
-        self,
-        http_conn_id=None,
-        webhook_token=None,
-        message="",
-        attachments=None,
-        blocks=None,
-        channel=None,
-        username=None,
-        icon_emoji=None,
-        icon_url=None,
-        link_names=False,
-        proxy=None,
-        *args,
-        **kwargs
-    ):
+    @apply_defaults  # type: ignore [misc]
+    def __init__(self, *, message: str = "", channel: Optional[str] = None, **kwargs: Any) -> None:
+        """Initialize LogMessageOperator."""
         self.message = message
         self.channel = channel
-        super().__init__(*args, **kwargs)
 
-    def execute(self, context):
+        # Only pass along arguments that the BaseOperator super class defines.
+        # Reason being that by default BaseOperator does not accept arguments it didn't
+        # define. Eg arguments that are specific to the class we are proxying, the
+        # SlackWebhookOperator.
+        sig = inspect.signature(BaseOperator.__init__)
+        params = set(sig.parameters.keys())
+
+        # key views are set-like: https://docs.python.org/3.8/library/stdtypes.html#dict-views
+        super_kwargs = {k: kwargs[k] for k in params & kwargs.keys()}
+        super().__init__(**super_kwargs)
+
+    def execute(self, context: Context) -> None:
+        """Log message to default logger."""
         self.log.info("%s for channel %s", self.message, self.channel)
