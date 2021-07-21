5461cfe977ba01f401c6812a0365083021bc59a9
Ron van Barneveld <r.van.barneveld@amsterdam.nl>
2021-07-21 14:48:40 +0200

    Aanpassing afval airflow flow

--- src/dags/huishoudelijkafval.py
+++ src/dags/huishoudelijkafval.py
@@ -3,42 +3,21 @@ from typing import Dict, List, Union
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from common import (
-    DATAPUNT_ENVIRONMENT,
-    DATASTORE_TYPE,
-    MessageOperator,
-    default_args,
-    slack_webhook_token,
-)
+from common import DATAPUNT_ENVIRONMENT, MessageOperator, default_args, slack_webhook_token
 from common.sql import SQL_CHECK_COUNT
 from contact_point.callbacks import get_contact_point_on_failure_callback
 from importscripts.import_afvalinzamelingplanning import load_from_dwh
 from pgcomparator_cdc_operator import PgComparatorCDCOperator
 from postgres_check_operator import PostgresCheckOperator
 from postgres_permissions_operator import PostgresPermissionsOperator
-from provenance_drop_from_schema_operator import ProvenanceDropFromSchemaOperator
 from provenance_rename_operator import ProvenanceRenameOperator
 from schematools.utils import to_snake_case
 from sql.afvalinzamelingplanning import SQL_DROP_TMP_TABLE
 from sqlalchemy_create_object_operator import SqlAlchemyCreateObjectOperator
-from swap_schema_operator import SwapSchemaOperator
-from swift_load_sql_operator import SwiftLoadSqlOperator
 
 owner = "team_ruimte"
 dag_id: str = "huishoudelijkafval"
 tables: Dict[str, Union[List[str], str]] = {
-    "dump_file": [
-        "container",
-        "containerlocatie",
-        "cluster",
-        "containertype",
-        "weging",
-        "clusterfractie",
-        "loopafstandCategorie",
-        "bagObjectLoopafstand",
-        "adresLoopafstand",
-        "bijplaatsingen",
-    ],
     "dwh_stadsdelen": "planningVoertuigen",
 }
 
@@ -59,62 +38,25 @@ with DAG(
         username="admin",
     )
 
-    # 2. DUMP FILE SOURCE
-    # Drop tables in target schema PTE
-    # (schema which orginates from the DB dump file, see next step)
-    # based upon presence in the Amsterdam schema definition
-    drop_tables = ProvenanceDropFromSchemaOperator(
-        task_id="drop_tables",
-        dataset_name="huishoudelijkafval",
-        pg_schema="pte",
-    )
-
-    # 3. DUMP FILE SOURCE
-    # load the dump file
-    load_file = SwiftLoadSqlOperator(
-        task_id="load_dump_file",
-        container="Dataservices",
-        object_id=f"afval_huishoudelijk/{DATASTORE_TYPE}/" "afval_api.zip",
-        swift_conn_id="objectstore_dataservices",
-        # optionals
-        # db_target_schema will create the schema if not present
-        db_target_schema="pte",
-    )
-
-    # 4. DUMP FILE SOURCE
-    # Make the provenance translations
-    provenance_file_data = ProvenanceRenameOperator(
-        task_id="provenance_file",
-        dataset_name="huishoudelijkafval",
-        subset_tables=f"{tables['dump_file']}",
-        pg_schema="pte",
-        rename_indexes=True,
-    )
-
-    # 5. DUMP FILE SOURCE
-    # Swap tables to target schema public
-    swap_schema = SwapSchemaOperator(
-        task_id="swap_schema", dataset_name="huishoudelijkafval", subset_tables=tables["dump_file"]
-    )
-
-    # 6. DWH STADSDELEN SOURCE
+    # 2. DWH STADSDELEN SOURCE
     # Load voertuigenplanning data into DB
     load_dwh = PythonOperator(
-        task_id="load_from_dwh_stadsdelen",
+        task_id="load_voertuigenplanning_data",
         python_callable=load_from_dwh,
         op_args=[f"{dag_id}_{to_snake_case(tables['dwh_stadsdelen'])}_new"],
     )
 
-    # 7. Check minimum number of records
+    # 3. Check minimum number of records
     check_count = PostgresCheckOperator(
         task_id="check_count",
         sql=SQL_CHECK_COUNT,
-        params=dict(
-            tablename=f"{dag_id}_{to_snake_case(tables['dwh_stadsdelen'])}_new", mincount=1000
-        ),
+        params={
+            "tablename": f"{dag_id}_{to_snake_case(tables['dwh_stadsdelen'])}_new",
+            "mincount": 1000,
+        },
     )
 
-    # 8. DWH STADSDELEN SOURCE
+    # 4. DWH STADSDELEN SOURCE
     # Rename COLUMNS based on provenance (if specified)
     provenance_dwh_data = ProvenanceRenameOperator(
         task_id="provenance_dwh",
@@ -126,7 +68,7 @@ with DAG(
         pg_schema="public",
     )
 
-    # 9. DWH STADSDELEN SOURCE
+    # 5. DWH STADSDELEN SOURCE
     # Create the DB target table (as specified in the JSON data schema)
     # if table not exists yet
     create_tables = SqlAlchemyCreateObjectOperator(
@@ -138,7 +80,7 @@ with DAG(
         ind_extra_index=True,
     )
 
-    # 10. DWH STADSDELEN SOURCE
+    # 6. DWH STADSDELEN SOURCE
     # Check for changes to merge in target table by using CDC
     change_data_capture = PgComparatorCDCOperator(
         task_id="change_data_capture",
@@ -149,39 +91,35 @@ with DAG(
         use_key=True,
     )
 
-    # 11. DWH STADSDELEN SOURCE
+    # 7. DWH STADSDELEN SOURCE
     # Clean up; delete temp table
     clean_up = PostgresOperator(
         task_id="clean_up",
         sql=SQL_DROP_TMP_TABLE,
-        params=dict(tablename=f"{dag_id}_{to_snake_case(tables['dwh_stadsdelen'])}_new"),
+        params={"tablename": f"{dag_id}_{to_snake_case(tables['dwh_stadsdelen'])}_new"},
     )
 
-    # 12. Grant database permissions
+    # 8. Grant database permissions
     grant_db_permissions = PostgresPermissionsOperator(task_id="grants", dag_name=dag_id)
 
 
 # FLOW
-slack_at_start >> [drop_tables, load_dwh]
-# Path 1
-[drop_tables >> load_file >> provenance_file_data >> swap_schema]
-# Path 2
-[
-    load_dwh
+(
+    slack_at_start
+    >> load_dwh
     >> check_count
     >> provenance_dwh_data
     >> create_tables
     >> change_data_capture
     >> clean_up
-]
-
-[swap_schema, clean_up] >> grant_db_permissions
-
+)
+# [swap_schema, clean_up] >> grant_db_permissions
+[clean_up] >> grant_db_permissions
 dag.doc_md = """
     #### DAG summary
     This DAG processes data about waste objects and related context execution planning
     of waste collection.
-    A part of the data orginates from team Ruimte by means of a dump file
+    A part of the data orginates is push by team Ruimte to the referentiedatabase
     A part of the data orginates from DWH stadsdelen by means of a database connection
     #### Mission Critical
     Classified as 2 (beschikbaarheid [range: 1,2,3])
