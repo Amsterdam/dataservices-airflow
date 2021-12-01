e305305ab6e8ce24309a2e8b6b4a355497674205
C van Riel <c.van.riel@amsterdam.nl>
2021-12-01 13:42:12 +0100

    test

---
+++ src/dags/brp_iburgerzaken.py
@@ -0,0 +1,90 @@
+from datetime import timedelta
+from pathlib import Path
+from typing import Final
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.kubernetes.secret import Secret
+from common import (
+    DATAPUNT_ENVIRONMENT,
+    SHARED_DIR,
+    MessageOperator,
+    default_args,
+    slack_webhook_token,
+)
+from contact_point.callbacks import get_contact_point_on_failure_callback
+from importscripts.import_brp_iburgerzaken import container_variables
+
+# SETUP the use of mounting secrets instead of using environment variables (the latter
+# being prone for unwanted secret exposure if someone can describe the pod)
+#  GENERIC_VARS_NAMES:list = ['ST_IBURGERZAKEN_CONTAINER_NAME', 'ST_IBURGERZAKEN_CONNECTION_STRING',
+#                             'DB_IBURGERZAKEN_SERVER', 'DB_IBURGERZAKEN_DB_NAME', 'DB_IBURGERZAKEN_DB_UID',
+#                             'DB_IBURGERZAKEN_DB_UID_PWD', 'AIRFLOW__CORE__SQL_ALCHEMY_CONN']
+# https://github.com/VBhojawala/airflow/blob/k8s-docs/docs/apache-airflow-providers-cncf-kubernetes/operators.rst#mounting-secrets-as-volume
+# The instantiating of Secret needs a type (volume or env), a location path and the object secret name as stated in the values.yml in HELM.
+secret_file = Secret("volume", "/TMP/secrets", "db-iburgerzaken-db-uid-pwd")
+# secret_file2 = Secret('volume', '/TMP/secrets', 'db-iburgerzaken-server')
+# secret_env  = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
+
+# SETUP GENERAL DAG RELATED VARIABLES
+CONTAINER_IMAGE: Final = "crdaveontweu01.azurecr.io/airflow-workload-iburgerzaken:latest"  # [registry]/[imagename]:[tag]
+COMMAND_TO_EXECUTE: list = ["python"]  # Command that you want to run on container start
+COMMAND_ARGS: list = [
+    "/scripts/logic.py"
+]  # Command arguments that will be used with command to execute on start
+DATATEAM_OWNER: Final = "datateam_basis_kernregistraties"
+DAG_ID: Final = "brp_iburgerzaken"
+DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
+TMP_DIR: Final = Path(SHARED_DIR) / DAG_ID
+K8_NAME_SPACE: Final = "airflow"
+
+# SETUP CONTAINER SPECIFIC ENV VARS
+CONTAINERS_TO_RUN_IN_PARALLEL: dict[str, dict] = container_variables()
+
+with DAG(
+    DAG_ID,
+    description="""Running a containerized workload that collects BRP
+                    data from source iBurgerzaken.""",
+    default_args=default_args,
+    template_searchpath=["/"],
+    on_failure_callback=get_contact_point_on_failure_callback(dataset_id=DAG_ID),
+    catchup=False,
+) as dag:
+
+    # 1. Post info message on slack
+    slack_at_start = MessageOperator(
+        task_id="slack_at_start",
+        http_conn_id="slack",
+        webhook_token=slack_webhook_token,
+        message=f"Starting {DAG_ID} ({DATAPUNT_ENVIRONMENT})",
+        username="admin",
+    )
+
+    # 2. Excuting containers
+    # The KubernetesPodOperator enables you to run containerized workloads as pods on Kubernetes from your DAG.
+    procesdata = [
+        KubernetesPodOperator(
+            task_id=container_name,
+            namespace=K8_NAME_SPACE,
+            image=CONTAINER_IMAGE,
+            cmds=COMMAND_TO_EXECUTE,
+            arguments=COMMAND_ARGS,
+            labels=DAG_LABEL,
+            env_vars=container_vars,
+            name=DAG_ID,
+            image_pull_policy="Always",
+            get_logs=True,
+            in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
+            is_delete_operator_pod=False,  # if true delete pod when pod reaches its final state.
+            log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
+            hostnetwork=True,  # If True enable host networking on the pod.
+            secrets=[secret_file,], # Uses a mount to get to secret
+            reattach_on_restart=True,
+            dag=dag,
+            execution_timeout=timedelta(hours=4) # to prevent taks becoming marked as failed when taking longer
+        )
+        for container_name, container_vars in CONTAINERS_TO_RUN_IN_PARALLEL.items()
+    ]
+
+# FLOW
+slack_at_start >> procesdata
