9426fe65ce2ccc2c6cb02d79ad6c9dc925bd012d
C van Riel <c.van.riel@amsterdam.nl>
2021-12-09 11:29:52 +0100

    test

--- src/dags/brp_iburgerzaken.py
+++ src/dags/brp_iburgerzaken.py
@@ -35,7 +35,7 @@ DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
 TMP_DIR: Final = Path(SHARED_DIR) / DAG_ID
 K8_NAME_SPACE: Final = "airflow-dave"
 K8_NODE_POOL: Final = [
-    "airflow",
+    "airflowbenk",
 ]
 
 # SETUP CONTAINER SPECIFIC ENV VARS
