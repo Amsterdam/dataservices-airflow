d8e047bb66fd6c1d78009c94f0e34d3f7f9e4b6d
C van Riel <c.van.riel@amsterdam.nl>
2021-12-16 10:15:32 +0100

    test

--- src/dags/brp_iburgerzaken.py
+++ src/dags/brp_iburgerzaken.py
@@ -1,11 +1,9 @@
 import os
-from datetime import timedelta
 from pathlib import Path
 from typing import Final
 
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.operators.dummy import DummyOperator
 from airflow.kubernetes.secret import Secret
 from common import (
     DATAPUNT_ENVIRONMENT,
@@ -80,13 +78,17 @@ with DAG(
             labels=DAG_LABEL,
             env_vars=container_vars,
             name=DAG_ID,
+            # Determines when to pull a fresh image, if 'IfNotPresent' will cause
+            # the Kubelet to skip pulling an image if it already exists. If you
+            # want to always pull a new image, set it to 'Always'.
             image_pull_policy="Always",
             # Known issue in the KubernetesPodOperator
             # https://stackoverflow.com/questions/55176707/airflow-worker-connection-broken-incompleteread0-bytes-read
             # set get_logs to false
+            # If true, logs stdout output of container. Defaults to True.
             get_logs=False,
             in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
-            is_delete_operator_pod=False,  # if true delete pod when pod reaches its final state.
+            is_delete_operator_pod=True,  # if true delete pod when pod reaches its final state.
             log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
             hostnetwork=False,  # If True enable host networking on the pod.
             secrets=[
@@ -94,19 +96,27 @@ with DAG(
             ],  # Uses a mount to get to secret
             reattach_on_restart=True,
             dag=dag,
+            # Timeout to start up the Pod, default is 120.
             startup_timeout_seconds=3600,
             # execution_timeout=timedelta(
             #     hours=4
             # ),  # to prevent taks becoming marked as failed when taking longer
+            # Select a specific nodepool to use. Could also be specified by affinity.
+            node_selector={'nodetype': AKS_NODE_POOL},
             # Resource specifications for Pod, this will allow you to set both cpu
             # and memory limits and requirements.
-            # resources={'limit_memory': "250M", 'limit_cpu': "100m"},
-            node_selector={'nodetype': AKS_NODE_POOL},
             resources={
                 'request_memory': '1Gi',
                 'request_cpu': 2,
                 'limit_memory': '4Gi',
                 'limit_cpu': 4},
+            # List of Volume objects to pass to the Pod.
+            volumes=[],
+            # List of VolumeMount objects to pass to the Pod.
+            volume_mounts=[],
+            # Affinity determines which nodes the Pod can run on based on the
+            # config. For more information see:
+            # https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
             # affinity={
             #     "nodeAffinity": {
             #         # requiredDuringSchedulingIgnoredDuringExecution means in order
