f31e226069602897eb946160d7656913d3090422
C van Riel <c.van.riel@amsterdam.nl>
2021-12-19 11:39:37 +0100

    test

--- src/dags/brp_iburgerzaken.py
+++ src/dags/brp_iburgerzaken.py
@@ -4,6 +4,7 @@ from typing import Final
 
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.kubernetes.secret import Secret
 from common import (
     DATAPUNT_ENVIRONMENT,
@@ -64,7 +65,7 @@ with DAG(
 
     # 2. Excuting containers
     # The KubernetesPodOperator enables you to run containerized workloads as pods on Kubernetes from your DAG.
-    procesdata = [
+    procesdata_header = [
         KubernetesPodOperator(
             task_id=container_name,
             namespace=AKS_NAMESPACE,
@@ -143,10 +144,66 @@ with DAG(
             #     }
             # },
         )
-        for container_name, container_vars in CONTAINERS_TO_RUN_IN_PARALLEL.items()
+        for container_name, container_vars in CONTAINERS_TO_RUN_IN_PARALLEL.items() if '_1' in container_name
     ]
 
-    # # 2. Excuting containers
+    # 3. Dummy operator acts as an interface between parallel tasks to another parallel tasks with different number of lanes
+    #  (without this intermediar, Airflow will give an error)
+    Interface = DummyOperator(task_id="interface")
+
+    # 4. Excuting containers
+    # The KubernetesPodOperator enables you to run containerized workloads as pods on Kubernetes from your DAG.
+    procesdata_other = [
+        KubernetesPodOperator(
+            task_id=container_name,
+            namespace=AKS_NAMESPACE,
+            image=CONTAINER_IMAGE,
+            cmds=COMMAND_TO_EXECUTE,
+            arguments=COMMAND_ARGS_PROCES,
+            labels=DAG_LABEL,
+            env_vars=container_vars,
+            name=DAG_ID,
+            # Determines when to pull a fresh image, if 'IfNotPresent' will cause
+            # the Kubelet to skip pulling an image if it already exists. If you
+            # want to always pull a new image, set it to 'Always'.
+            image_pull_policy="Always",
+            # Known issue in the KubernetesPodOperator
+            # https://stackoverflow.com/questions/55176707/airflow-worker-connection-broken-incompleteread0-bytes-read
+            # set get_logs to false
+            # If true, logs stdout output of container. Defaults to True.
+            get_logs=False,
+            in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
+            is_delete_operator_pod=True,  # if true delete pod when pod reaches its final state.
+            log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
+            hostnetwork=False,  # If True enable host networking on the pod.
+            secrets=[
+                secret_file,
+            ],  # Uses a mount to get to secret
+            reattach_on_restart=True,
+            dag=dag,
+            # Timeout to start up the Pod, default is 120.
+            startup_timeout_seconds=3600,
+            # execution_timeout=timedelta(
+            #     hours=4
+            # ),  # to prevent taks becoming marked as failed when taking longer
+            # Select a specific nodepool to use. Could also be specified by affinity.
+            node_selector={'nodetype': AKS_NODE_POOL},
+            # Resource specifications for Pod, this will allow you to set both cpu
+            # and memory limits and requirements.
+            resources={
+                'request_memory': '1Gi',
+                'request_cpu': 2,
+                'limit_memory': '4Gi',
+                'limit_cpu': 4},
+            # List of Volume objects to pass to the Pod.
+            volumes=[],
+            # List of VolumeMount objects to pass to the Pod.
+            volume_mounts=[],
+        )
+        for container_name, container_vars in CONTAINERS_TO_RUN_IN_PARALLEL.items() if '_1' not in container_name
+    ]
+
+    # # 2. Import data into SQLlite3
     # # The KubernetesPodOperator enables you to run containerized workloads as pods on Kubernetes from your DAG.
     # mergedata = KubernetesPodOperator(
     #         task_id='merge_data',
@@ -199,6 +256,6 @@ with DAG(
 
 # FLOW
 (
-    slack_at_start >> procesdata
+    slack_at_start >> procesdata_header >> Interface >> procesdata_other
 
 )
