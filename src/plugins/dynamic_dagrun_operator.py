9d0f2e01a81fad241356c02a0ae1d05e2417c5c6
jjmurre <jan.murre@gmail.com>
2022-01-13 21:25:32 +0100

    Refactor scheduling for GOB dag
    The GOB dag is using an operator that triggers the next
    DAG as a final step in the DAG run. This makes all the dynamically
    generated GOB dags run in some kind of "estafette" fashion,
    strictly serializing the DAGs. The reason for this is that
    the GOB API cannot handle much load.
    
    However, not all datasets need to run according to the same schedule.
    So, now it is possible to define several schedules and group
    the datasets that are using the same schedule.
    
    The configuration of this schedule-to-datasets mapping is
    done in the `GOB_SCHEDULES` enviroment variable.
    
    The DynamicDagRun operator (that triggers the next DAG) has been changed.
    The previous version was only able to trigger the next dag in a
    series of DAGs with a common prefix.
    
    Now, the operator can be configured with a `labels_group`.
    These labels are used make a subselection of the set of dags
    with a common prefix.

--- src/plugins/dynamic_dagrun_operator.py
+++ src/plugins/dynamic_dagrun_operator.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from typing import Any, Optional
 
-from airflow.models.dag import DagModel, DagRun
+from airflow.models.dag import DagModel, DagTag
+from airflow.models.dagrun import DagRun
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.settings import Session
-from airflow.utils.decorators import apply_defaults
 from airflow.utils.state import State
 from sqlalchemy import not_
 
@@ -27,10 +27,10 @@ class TriggerDynamicDagRunOperator(TriggerDagRunOperator):
     When the next DAGs in the chain is still running, it will not be triggered!
     """
 
-    @apply_defaults
     def __init__(
         self,
         dag_id_prefix: str = "",
+        labels_group: Optional[set[str]] = None,
         execution_date: Optional[datetime] = None,
         *args: Any,
         **kwargs: Any,
@@ -40,35 +40,41 @@ class TriggerDynamicDagRunOperator(TriggerDagRunOperator):
         Args:
             dag_id_prefix: Prefix that bundles a series of DAGs that need to run
                 in a serialized (estafette) fashion.
+            labels_group: Set of labels representing dataset that are grouped
+                in the same schedule.
             execution_date: execution date for the dag (templated)
         """
         super().__init__(
-            trigger_dag_id=None,
+            trigger_dag_id="",
             execution_date=execution_date,
             *args,
             **kwargs,
         )
         assert len(dag_id_prefix) > 0, "A dag_id_prefix is mandatory to use this Operator."
         self.dag_id_prefix = dag_id_prefix
+        self.labels_group = labels_group
 
     def execute(self, context: dict[str, Any]) -> None:
+        """Executes the Operator."""
         # Do not trigger next dag when param no_next_dag is available
         # Due to bug in Airflow, dagrun misses 'conf' attribute
         # when DAG is triggered from another DAG
         dag_run = context["dag_run"]
-        if dag_run is not None and (getattr(dag_run, "conf") or {}).get("no_next_dag"):
+        if dag_run is not None and (dag_run.conf or {}).get("no_next_dag"):
             self.log.info("Not starting next dag ('no_next_dag' in dag_run config)!")
             return
         current_dag_id = self.dag.dag_id
         self.log.info("Starting dag %s", current_dag_id)
         session = Session()
-        active_dag_ids = [
-            d.dag_id
-            for d in session.query(DagModel)
+        query = (
+            session.query(DagModel)
             .filter(not_(DagModel.is_paused))
             .filter(DagModel.dag_id.like(f"{self.dag_id_prefix}%"))
-            .order_by("dag_id")
-        ]
+        )
+        if self.labels_group is not None:
+            query = query.filter(DagModel.tags.any(DagTag.name.in_(self.labels_group)))
+
+        active_dag_ids = [d.dag_id for d in query.order_by("dag_id")]
 
         try:
             current_dag_idx = active_dag_ids.index(current_dag_id)
