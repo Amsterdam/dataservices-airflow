92d12be02e3ed43a8b663f960d32969e3747ac9c
C van Riel <c.van.riel@amsterdam.nl>
2022-02-14 16:54:50 +0100

    rename zone zwaarverkeer to zone zwaar verkeer

--- src/plugins/postgres_permissions_operator.py
+++ src/plugins/postgres_permissions_operator.py
@@ -40,7 +40,7 @@ DAG_DATASET: Final = {
     "reclamebelasting": "belastingen",
     "processenverbaalverkiezingen": "verkiezingen",
     "financien_": "financien",
-    "wegenbestand": "wegenbestandZoneZwaarverkeer",
+    "wegenbestand": "wegenbestandZoneZwaarVerkeer",
 }
 
 
