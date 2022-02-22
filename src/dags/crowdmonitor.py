acfea98999d1c6ed9353fb854c1e9bfdcb7a9629
Hielke <kramerh@gmail.com>
2022-02-22 14:32:32 +0100

    Reduce aantal_passanten to zero if < 15

--- src/dags/crowdmonitor.py
+++ src/dags/crowdmonitor.py
@@ -70,7 +70,10 @@ def copy_data_from_dbwaarnemingen_to_masterdb() -> None:
                    'uur' AS periode,
                    s.location_name AS naam_locatie,
                    v.datum_uur,
-                   v.aantal_passanten,
+                   CASE
+                       WHEN v.aantal_passanten < 15 THEN 0
+                       ELSE v.aantal_passanten
+                   END AS aantal_passanten
                    s.gebied,
                    ST_Transform(s.geom, 28992) AS geometrie  -- Ref DB only uses SRID 28992
                 FROM cmsa v
