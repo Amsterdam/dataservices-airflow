5fef9b8cc6ca1422447e34c8a04c07724a987da4
C van Riel <c.van.riel@amsterdam.nl>
2022-04-09 14:50:13 +0200

    add structured logging to Airflow

---
+++ src/custom_logging/logging_formatter.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pythonjsonlogger import jsonlogger
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    """A custom JSON formater based on Python pythonjsonlogger."""
+
+    def add_fields(self, log_record: dict, record: dict, message_dict: dict) -> None:
+        """Overwriting out of the log fields to be logged.
+
+        Which attributes to log are defined in the logging_handler.py
+        This method can overwrite he default values for those attributes if needed.
+        """
+        super().add_fields(log_record, record, message_dict)
+        if not log_record.get("timestamp"):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            log_record["timestamp"] = now
+        if log_record.get("level"):
+            log_record["level"] = log_record["level"].upper()
+        else:
+            log_record["level"] = record.get("levelname")
