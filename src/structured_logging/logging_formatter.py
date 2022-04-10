c72010b8fa81400ca7931a5c6cb67c0bc5c59384
C van Riel <c.van.riel@amsterdam.nl>
2022-04-10 13:48:55 +0200

    updated readme.md about logging

--- src/structured_logging/logging_formatter.py
+++ src/structured_logging/logging_formatter.py
@@ -14,10 +14,12 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
         This method can overwrite he default values for those attributes if needed.
         """
         super().add_fields(log_record, record, message_dict)
+
         if not log_record.get("timestamp"):
             # this doesn't use record.created, so it is slightly off
             now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
             log_record["timestamp"] = now
+
         if log_record.get("level"):
             log_record["level"] = log_record["level"].upper()
         else:
