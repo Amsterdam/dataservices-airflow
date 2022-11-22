20f7c3a66e754cb5354b6d7b9ade09c8e31f807a
C van Riel <c.van.riel@amsterdam.nl>
2022-11-22 14:06:38 +0100

    bugfix schema-tools for GOB dags

--- src/plugins/http_gob_operator.py
+++ src/plugins/http_gob_operator.py
@@ -252,12 +252,6 @@ class HttpGobOperator(BaseOperator):
                         request_end_time - request_start_time,
                         cursor_pos,
                     )
-                    # TODO: remove the lines below
-                    shutil.copy(
-                        tmp_file,
-                        f"{SHARED_DIR}/{dataset_table_id}-{datetime.now().isoformat()}.ndjson",
-                    )
-                    # END
                     last_record = importer.load_file(
                         tmp_file, is_through_table=self.is_through_table
                     )
