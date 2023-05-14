3ca6559a1b6fb524a8f642384e71e6c37bb1fd41
C van Riel <c.van.riel@amsterdam.nl>
2023-05-14 17:24:02 +0200

    prevent duplicate rows from stopping processing

--- src/dags/sql/wior.py
+++ src/dags/sql/wior.py
@@ -33,3 +33,27 @@ ALTER TABLE {{ params.tablename }}
 ALTER COLUMN datum_start_uitvoering TYPE date USING datum_start_uitvoering::date,
 ALTER COLUMN datum_einde_uitvoering TYPE date USING datum_einde_uitvoering::date;
 """
+
+# Due to source data errors, duplicate `ID` values can occur. The source
+# maintainer should fix the data.
+# To prevent the discontinuation of the whole data set, the duplicate rows
+# are removed from processing.
+# The `ctid` is used as the native internal identifier to remove a duplicate
+# row.
+# The duplicates are ordered based on validity of their geometry. True first,
+# false last.
+SQL_DEL_DUPLICATE_ROWS: Final = """
+    WITH duplicates AS (
+    SELECT
+    CTID,
+    ROW_NUMBER () OVER (PARTITION BY {{ params.dupl_id_col }}
+                        ORDER BY NOT ST_IsValid({{ params.geom_col }})) dulicate_num,
+    ST_IsValid({{ params.geom_col }}) ind_valid
+    FROM {{ params.tablename }}
+    )
+    DELETE FROM {{ params.tablename }}
+    WHERE CTID in (
+            SELECT CTID FROM duplicates
+            WHERE ind_valid = false OR dulicate_num > 1);
+    COMMIT;
+"""
