c11ee6538f2b3c3c0411fb15274d564840574cc9
C van Riel <c.van.riel@amsterdam.nl>
2023-09-11 17:45:06 +0200

    exclude _az dags in permissions operator

--- src/plugins/postgres_permissions_operator.py
+++ src/plugins/postgres_permissions_operator.py
@@ -143,7 +143,7 @@ class PostgresPermissionsOperator(BaseOperator):
 
                 # get list of dags that meet time window and state outcome
                 # it uses the Airflow DagRun class to get data
-                executed_dags_after_delta = [
+                executed_dags_after_delta = {
                     dag.dag_id
                     for dag in session.query(DagRun)
                     .filter(DagRun.end_date > delta)
@@ -156,7 +156,7 @@ class PostgresPermissionsOperator(BaseOperator):
                     .filter(DagRun.dag_id != "airflow_log_cleanup")
                     # exclude _az dags, since they are not present in the refDB on cVPS and do not need any permissions set.
                     .filter(DagRun.dag_id.notlike("%_az%"))
-                ]
+                }
 
                 if executed_dags_after_delta:
 
